ee0e536d0233593aeaa2f64c9582bddb39c8aad9
lumos! <43442549+LumosLiang@users.noreply.github.com>
2020-11-16 18:23:13 +0800

    84 ms

---
+++ solutions/1302. Deepest Leaves Sum.py
@@ -0,0 +1,14 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+​
+# method1: find the level and level traversal, iterative
+# method2: DFS, store the level and value, recursive
+​
+class Solution:
+    def deepestLeavesSum(self, root: TreeNode) -> int:
+        
+        
