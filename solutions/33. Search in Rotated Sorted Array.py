53c1574b8529e96dbd35132a6875149bd856ec1d
lumos! <43442549+LumosLiang@users.noreply.github.com>
2020-11-26 19:01:06 +0800

    44 ms

--- solutions/33. Search in Rotated Sorted Array.py
+++ solutions/33. Search in Rotated Sorted Array.py
@@ -1,43 +1,13 @@
 class Solution:
     def search(self, nums, target):
          
-        def findmin(nums):
-            low, high = 0, len(nums) - 1
 ​
-            while (low <= high):
-                if low == high:
-                    return low
-​
-                mid = low + (high - low) // 2
-                if nums[mid] < nums[mid - 1]:
-                    return mid
-                else:
-                    if nums[mid] > nums[high]:
-                        low = mid + 1
-                        continue
-                    elif nums[mid] < nums[low]:
-                        high = mid - 1
-                        continue
-                    return low
-        
-        def binary_search(arr, start, end, target):
-            if start > end:
-                return -1
-            mid = start + (end - start) // 2
-            if arr[mid] > target:
-                return binary_search(arr, start, mid - 1, target)
-            if arr[mid] < target:
-                return binary_search(arr, mid + 1, end, target)
-            return mid
-        
-        
-        min_idx = findmin(nums)
-        if target > nums[-1]:
-            return binary_search(nums[:min_idx], 0, min_idx - 1 , target)
-        else:
-            res = binary_search(nums[min_idx:], 0, len(nums) - 1 - min_idx, target)
-            if res == -1:
-                return res
+        lo, hi = 0, len(nums) - 1
+        while lo < hi:
+            mid = (lo + hi) // 2
+            if (nums[0] > target) ^ (nums[0] > nums[mid]) ^ (target > nums[mid]):
+                lo = mid + 1
             else:
-                return res+min_idx
+                hi = mid
+        return lo if target in nums[lo:lo+1] else -1
 ​
