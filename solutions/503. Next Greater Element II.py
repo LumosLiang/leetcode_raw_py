fc91381be0bf2faa3cd34874cd016ebc896f0d06
lumos! <43442549+LumosLiang@users.noreply.github.com>
2020-11-30 15:40:37 +0800

    3932 ms

---
+++ solutions/503. Next Greater Element II.py
@@ -0,0 +1,9 @@
+class Solution:
+    def nextGreaterElements(self, A):
+        stack, res = [], [-1] * len(A)
+        for i in range(len(A)) * 2:
+            while stack and (A[stack[-1]] < A[i]):
+                res[stack.pop()] = A[i]
+            stack.append(i)
+        return res
+    
