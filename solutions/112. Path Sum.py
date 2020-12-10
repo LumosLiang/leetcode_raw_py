aafd78e6549c47d27764bc1c160484460196493f
lumos! <43442549+LumosLiang@users.noreply.github.com>
2020-12-10 15:58:01 +0800

    40 ms

--- solutions/112. Path Sum.py
+++ solutions/112. Path Sum.py
@@ -9,5 +9,12 @@
 ​
 class Solution:
     def hasPathSum(self, root: TreeNode, sum: int) -> bool:
+        if root is None: return False
         
+        if root.val == sum and root.left is None and root.right is None:
+            return True
+        
+        return self.hasPathSum(root.left, sum - root.val) or self.hasPathSum(root.right, sum - root.val)
+             
+           
 ​
