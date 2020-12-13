29410b239f20aebe2f088c12919c3a8b0622a0e4
lumos! <43442549+LumosLiang@users.noreply.github.com>
2020-12-13 23:34:15 +0800

    36 ms

--- solutions/104. Maximum Depth of Binary Tree.py
+++ solutions/104. Maximum Depth of Binary Tree.py
@@ -7,3 +7,14 @@
 class Solution:
     def maxDepth(self, root: TreeNode) -> int:
         
+        if root is None: return 0
+        
+        if root.left and root.right:
+            return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1
+        elif not root.left and not root.right:
+            return 1
+        elif root.left and not root.right:
+            return self.maxDepth(root.left) + 1
+        elif not root.left and root.right:
+            return self.maxDepth(root.right) + 1
+        
