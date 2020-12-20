e5b3cb92c75ab77cc0dcc40eb051a01725bc9311
lumos! <43442549+LumosLiang@users.noreply.github.com>
2020-12-20 17:30:13 +0800

    N/A

---
+++ solutions/50. Pow(x, n).py
@@ -0,0 +1,13 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        
+        if n == 0: return 1
+        if n == 1: return x
+        if n == -1: return 1/x
+    
+        if n > 1:
+            return self.myPow(x, n // 2) * self.myPow(x, n - n // 2)
+        if n < -1:
+            return self.myPow(x, n // 2) * self.myPow(x, n - n // 2)
+        
+        
