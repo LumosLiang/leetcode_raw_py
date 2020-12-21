ed35b1de44ea20b0a99f46f4aa5c8eff14541da1
lumos! <43442549+LumosLiang@users.noreply.github.com>
2020-12-21 23:25:38 +0800

    88 ms

---
+++ solutions/784. Letter Case Permutation.py
@@ -0,0 +1,23 @@
+class Solution:
+    def letterCasePermutation(self, S: str) -> List[str]:
+        res, ans = [], []
+        S = list(S)
+        letter_idx = [k for k, v in enumerate(S) if (ord(v) >= 65 and ord(v) <= 90) or (ord(v) >= 97 and ord(v) <= 122)]
+        
+        def backtrack(choices, path):
+            res.append(path)
+            
+            for i in range(len(choices)):
+                backtrack(choices[i + 1:], path + [choices[i]])
+        
+        backtrack(letter_idx, [])
+        
+        for r in res:
+            temp = S[:]
+            for j in r:
+                if  97 <= ord(temp[j]) <= 122:
+                    temp[j] = chr(ord(temp[j]) - 32)
+                elif  65 <= ord(temp[j]) <= 90:
+                    temp[j] = chr(ord(temp[j]) + 32)
+            ans.append(''.join(temp))
+        return ans
