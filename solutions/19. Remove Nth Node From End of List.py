5019de2c6536ec2899632b6a984a44d7afa76a8e
lumos! <43442549+LumosLiang@users.noreply.github.com>
2020-12-28 16:05:12 +0800

    36 ms

---
+++ solutions/19. Remove Nth Node From End of List.py
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+​
+class Solution:
+    def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
+        
+        
+        
+        dummy = ListNode(0)
+        dummy.next = head
+        fast = slow = dummy
+        for _ in range(n):
+            fast = fast.next
+        while fast and fast.next:
+            fast = fast.next
+            slow = slow.next
+        slow.next = slow.next.next
+        return dummy.next
+​
