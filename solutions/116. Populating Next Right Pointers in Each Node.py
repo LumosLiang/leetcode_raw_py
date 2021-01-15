458dbd620e6f335a434a23e8a586b63c65c965ad
lumos! <43442549+LumosLiang@users.noreply.github.com>
2021-01-15 17:17:31 +0800

    64 ms

--- solutions/116. Populating Next Right Pointers in Each Node.py
+++ solutions/116. Populating Next Right Pointers in Each Node.py
@@ -10,20 +10,10 @@ class Node:
 ​
 class Solution:
     def connect(self, root: 'Node') -> 'Node':
-        if root is None: return None
-        
-        stack = [root]
-        
-        while stack:
-            temp = []
-            while stack:
-                node = stack.pop(0)
-                if stack:node.next = stack[0]
-                else: node.next = None
-                    
-                if node.left: temp.append(node.left)
-                if node.right: temp.append(node.right)
-    
-            stack = temp
-            
-        return root 
+        head = root
+        if root and root.left:
+            root.left.next = root.right
+            root.right.next = root.next and root.next.left
+            self.connect(root.left)
+            self.connect(root.right)
+        return head
