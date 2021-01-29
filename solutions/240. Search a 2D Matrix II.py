fbdc209d313657bee8f0cb718bc4c4b788fd8043
lumos! <43442549+LumosLiang@users.noreply.github.com>
2021-01-29 14:54:36 +0800

    168 ms

--- solutions/240. Search a 2D Matrix II.py
+++ solutions/240. Search a 2D Matrix II.py
@@ -1,27 +1,19 @@
 class Solution:
     def searchMatrix(self, matrix, target):
         
-        if matrix.count([]) == len(matrix): return False
-​
-        mid_0 = 0 + (len(matrix) - 1 - 0) // 2
-        mid_1 = 0 + (len(matrix[0]) - 1 - 0) // 2
+        if not matrix: return False
         
-        if matrix[mid_0][mid_1] > target:
-            
-            matrix_diag = [m[:mid_1] for m in matrix[:mid_0]]
-            matrix_left = [m[:mid_1] for m in matrix[mid_0:]]
-            matrix_up = [m[mid_1:] for m in matrix[:mid_0]]
-            
-            return self.searchMatrix(matrix_diag, target) or self.searchMatrix(matrix_left, target) or self.searchMatrix(matrix_up, target)
+        l = len(matrix[0]) - 1
+        w = 0
         
-        elif matrix[mid_0][mid_1] < target:
+        while l >= 0 and w <= len(matrix) - 1:
+            if matrix[w][l] == target:
+                return True
+            elif matrix[w][l] > target:
+                l -= 1
+            elif matrix[w][l] < target:
+                w += 1
             
-            matrix_diag = [m[mid_1 + 1:] for m in matrix[mid_0 + 1:]]
-            matrix_right = [m[mid_1 + 1:] for m in matrix[:mid_0 + 1]]
-            matrix_down = [m[:mid_1+1] for m in matrix[mid_0 + 1:]]
-                  
-            return self.searchMatrix(matrix_diag, target) or self.searchMatrix(matrix_right, target) or self.searchMatrix(matrix_down, target)
-        else:
-            return True
         
         return False
+​
