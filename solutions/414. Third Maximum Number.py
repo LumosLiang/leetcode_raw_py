3e9d4541440ba40f9c6211bc6117206a7fc5b3bf
lumos! <43442549+LumosLiang@users.noreply.github.com>
2021-02-03 17:08:55 +0800

    52 ms

--- solutions/414. Third Maximum Number.py
+++ solutions/414. Third Maximum Number.py
@@ -1,3 +1,16 @@
+import heapq
 class Solution:
     def thirdMax(self, nums: List[int]) -> int:
+        
+        nums = list(set(nums))
+        if len(nums) < 3:
+            return max(nums)
+        else:
+            nums = [-v for v in nums]
+            heapq.heapify(nums)
+            for _ in range(3):
+                ans = heapq.heappop(nums)
+            return -ans
+        
+        
         
