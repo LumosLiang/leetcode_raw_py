8b2c5af7d0e65143c3bf30a74c647e990c3e8c56
lumos! <43442549+LumosLiang@users.noreply.github.com>
2021-02-08 16:15:07 +0800

    128 ms

--- solutions/701. Insert into a Binary Search Tree.py
+++ solutions/701. Insert into a Binary Search Tree.py
@@ -5,22 +5,10 @@
 #         self.left = left
 #         self.right = right
 ​
-# recusive
 class Solution:
     def insertIntoBST(self, root: TreeNode, val: int) -> TreeNode:
-       
-        if not root: return TreeNode(val)
         
-        if root.val > val:
-            if root.left:
-                self.insertIntoBST(root.left, val)
-            else:
-                root.left = TreeNode(val)
-        else:
-            if root.right:
-                self.insertIntoBST(root.left, val)
-            else:
-                root.right = TreeNode(val)
-        
+        if root is None: return TreeNode(val)
+        if root.val > val: root.left = self.insertIntoBST(root.left, val)
+        if root.val < val: root.right = self.insertIntoBST(root.right, val)
         return root
-         
