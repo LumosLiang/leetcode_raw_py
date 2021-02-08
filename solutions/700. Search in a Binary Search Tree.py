06354fd1830fd710af938cf8e76500bcf9ff92aa
lumos! <43442549+LumosLiang@users.noreply.github.com>
2021-02-08 15:51:07 +0800

    72 ms

--- solutions/700. Search in a Binary Search Tree.py
+++ solutions/700. Search in a Binary Search Tree.py
@@ -4,6 +4,15 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+​
 class Solution:
     def searchBST(self, root: TreeNode, val: int) -> TreeNode:
+        
+        if root is None: return None
+        
+        if root.val == val: return root
+        if root.val > val: return self.searchBST(root.left, val)
+        if root.val < val: return self.searchBST(root.right, val)
+        
+        
         
