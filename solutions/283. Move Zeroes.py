b71e0143b390c8e912492608bdd384ac8a3b2dc5
lumos! <43442549+LumosLiang@users.noreply.github.com>
2021-03-19 17:08:44 +0800

    44 ms

--- solutions/283. Move Zeroes.py
+++ solutions/283. Move Zeroes.py
@@ -6,23 +6,16 @@ class Solution:
         # p1 - first zero point
         # p2 - first none-zero point
         
-        p1, p2 = 0, 0 
+        slow, fast = 0, 0
         
-        while p2 < len(nums):
-            while p1 < len(nums) and nums[p1] != 0: 
-                p1 += 1
-            
-            p2 = p1 + 1
-            
-            while p2 < len(nums) and nums[p2] == 0:
-                p2 += 1
-                
-            if p1 < p2 < len(nums): nums[p1], nums[p2] = nums[p2], nums[p1]
-            p1 += 1
-            p2 += 1
-        
+        while fast < len(nums):
+            if nums[fast] != 0:
+                nums[slow] = nums[fast]
+                slow += 1
+            fast += 1
+        for i in range(slow, len(nums)):
+            nums[i] = 0
         return nums
-        
         
           
         
