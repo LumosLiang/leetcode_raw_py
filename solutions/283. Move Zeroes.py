31bfc44cb2434e0c7a3b46af182665d267466ed2
lumos! <43442549+LumosLiang@users.noreply.github.com>
2021-03-19 17:04:51 +0800

    N/A

--- solutions/283. Move Zeroes.py
+++ solutions/283. Move Zeroes.py
@@ -3,4 +3,26 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        # p1 - first zero point
+        # p2 - first none-zero point
+        
+        p1, p2 = 0, 0 
+        
+        while p2 < len(nums):
+            while p1 < len(nums) and nums[p1] != 0: 
+                p1 += 1
+            
+            p2 = p1 + 1
+            
+            while p2 < len(nums) and nums[p2] == 0:
+                p2 += 1
+                
+            if p1 < p2 < len(nums): nums[p1], nums[p2] = nums[p2], nums[p1]
+            p1 += 1
+            p2 += 1
+        
+        return nums
+        
+        
+          
         
