d3c8f927b765afc118cab1c8363e1ec2f0dcd49e
lumos! <43442549+LumosLiang@users.noreply.github.com>
2021-03-23 23:16:16 +0800

    644 ms

--- solutions/11. Container With Most Water.py
+++ solutions/11. Container With Most Water.py
@@ -6,15 +6,16 @@ class Solution:
         r = len(height) - 1
         
         while l <= r:
-            res = max(res, min(height[l], height[r]) * (r - l))
-            
+            v = min(height[l], height[r]) * (r - l)
+            if v > res: res = v
+            # move the curr lower height to a place where first reach the higher height
             temp = 0
             if height[l] <= height[r]:
-                while l + temp <= r and height[l + temp] <= height[r]:
+                while l + temp <= r and height[l + temp] <= height[l]:
                     temp += 1
                 l += temp
             elif height[l] > height[r]:
-                while r - temp >= l and height[r - temp] <= height[l]:
+                while r - temp >= l and height[r - temp] <= height[r]:
                     temp += 1
                 r -= temp
         return res
