44975d8f4f951506920f078e3608110c686edfa8
lumos! <43442549+LumosLiang@users.noreply.github.com>
2021-03-23 23:05:00 +0800

    Update 1. Two Sum.py

--- solutions/1. Two Sum.py
+++ solutions/1. Two Sum.py
@@ -1,21 +1,9 @@
-import collections
 class Solution:
-    def twoSum(self, nums, target):
-        dit = {}
-        for k,v in enumerate(nums):
-            dit[v] = k
-        dit = collections.OrderedDict(sorted(dit.items()))
-        nums.sort()
-        
-        l, r = 0, len(nums) - 1
-        while l < r:
-            s = nums[l] + nums[r]
-            if s == target:
-                return [dit[nums[l]], dit[nums[r]]]
-            elif s < target:
-                l += 1
-            else:
-                r -= 1
-        
-        return None
-​
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hash = {}
+        
+        for i in range(len(nums)):
+            if nums[i] in hash:
+                return [i, hash[nums[i]]]
+            else:
+                hash[target - nums[i]] = i
