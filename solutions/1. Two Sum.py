b159f914664025e4dde2e7b4d1093dd99148e98f
lumos! <43442549+LumosLiang@users.noreply.github.com>
2021-03-23 23:06:11 +0800

    44 ms

--- solutions/1. Two Sum.py
+++ solutions/1. Two Sum.py
@@ -1,9 +1,9 @@
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hash = {}
-        
-        for i in range(len(nums)):
-            if nums[i] in hash:
-                return [i, hash[nums[i]]]
-            else:
-                hash[target - nums[i]] = i
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hash = {}
+        
+        for i in range(len(nums)):
+            if nums[i] in hash:
+                return [i, hash[nums[i]]]
+            else:
+                hash[target - nums[i]] = i
