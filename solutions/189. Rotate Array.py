538c14b84241951e224c58a5f80d17ecd29803eb
lumos! <43442549+LumosLiang@users.noreply.github.com>
2021-03-24 22:31:49 +0800

    Update 189. Rotate Array.py

--- solutions/189. Rotate Array.py
+++ solutions/189. Rotate Array.py
@@ -1,6 +1,10 @@
 class Solution:
-    def rotate(self, nums: List[int], k: int) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
-        
+    def rotate(self, nums, k):
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        while k != 0:
+            nums.insert(0, nums[-1])
+            nums.pop(-1)
+            k -= 1
+        
