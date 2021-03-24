7f4c73bbf9e7c86c292497cd2d9e8a38a24c83a0
lumos! <43442549+LumosLiang@users.noreply.github.com>
2021-03-24 22:35:29 +0800

    24 ms

--- solutions/226. Invert Binary Tree.py
+++ solutions/226. Invert Binary Tree.py
@@ -4,6 +4,20 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+​
+# iterative
 class Solution:
     def invertTree(self, root: TreeNode) -> TreeNode:
+        if root is None: return
         
+        q = [root]
+        
+        while q:
+            temp = q.pop()
+            if temp.left:
+                q.insert(0, temp.left)
+            if temp.right:
+                q.insert(0, temp.right)
+            temp.left, temp.right = temp.right, temp.left
+        return root
+            
