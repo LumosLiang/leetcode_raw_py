4011fc7a827100ef277e45654d6f544e8cb912c6
lumos! <43442549+LumosLiang@users.noreply.github.com>
2021-03-24 21:59:49 +0800

    36 ms

--- solutions/88. Merge Sorted Array.py
+++ solutions/88. Merge Sorted Array.py
@@ -1,6 +1,25 @@
 class Solution:
-    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+    def merge(self, nums1, m, nums2, n):
         """
+        m: The number of elements initialized in nums1
+        n: The number of elements initialized in nums2
         Do not return anything, modify nums1 in-place instead.
         """
         
+        pointer_2 = 0
+        pointer_1 = 0
+​
+        while pointer_2 < n:
+            while pointer_1 < len(nums1):
+                if nums1[pointer_1] == 0:
+                    nums1.insert(pointer_1, nums2[pointer_2])
+                    nums1.pop(-1)
+                    break
+                elif nums2[pointer_2] > nums1[pointer_1]:
+                    pointer_1 += 1
+                elif nums2[pointer_2] <= nums1[pointer_1]:
+                    nums1.insert(pointer_1, nums2[pointer_2])
+                    nums1.pop(-1)
+                    break
+            pointer_2 += 1
+        nums1.sort()
