ec2d5f3c2a2b17c205620753bf66fb2426f79362
lumos! <43442549+LumosLiang@users.noreply.github.com>
2021-03-24 22:16:12 +0800

    Update 118. Pascal's Triangle.py

--- solutions/118. Pascal's Triangle.py
+++ solutions/118. Pascal's Triangle.py
@@ -1,3 +1,23 @@
 class Solution:
-    def generate(self, numRows: int) -> List[List[int]]:
-        
+    def generate(self, numRows):
+        """
+        print function
+        """ 
+        result = []
+        for i in range(numRows):
+            result.append(self.getRow(i))
+
+        return result    
+    
+    def getRow(self, rowIndex):
+        """
+        Use recursion, but this time move the recursion out of loop
+        """
+        if rowIndex == 0:
+            return [1]
+        lst = [1]
+        lastrow = self.getRow(rowIndex - 1)
+        for i in range(1, rowIndex):
+            lst.append(lastrow[i - 1] + lastrow[i])
+        lst.append(1)
+        return lst
