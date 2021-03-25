2155c989054c95a0ee42074411cc0035ac16ad18
lumos! <43442549+LumosLiang@users.noreply.github.com>
2021-03-25 23:31:13 +0800

    24 ms

--- solutions/961. N-Repeated Element in Size 2N Array.py
+++ solutions/961. N-Repeated Element in Size 2N Array.py
@@ -1,3 +1,10 @@
-class Solution:
-    def repeatedNTimes(self, A: List[int]) -> int:
-        
+class Solution(object):
+    def repeatedNTimes(self, A):
+        """
+        :type A: List[int]
+        :rtype: int
+        """
+        for k in xrange(1, 4):
+            for i in xrange(len(A) - k):
+                if A[i] == A[i+k]:
+                    return A[i]
