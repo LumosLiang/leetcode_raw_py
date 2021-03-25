90bc94519a045226448451b7990c965e382c63d3
lumos! <43442549+LumosLiang@users.noreply.github.com>
2021-03-25 23:24:35 +0800

    136 ms

--- solutions/852. Peak Index in a Mountain Array.py
+++ solutions/852. Peak Index in a Mountain Array.py
@@ -1,3 +1,9 @@
 class Solution:
-    def peakIndexInMountainArray(self, arr: List[int]) -> int:
-        
+    def peakIndexInMountainArray(self, A):
+        low = 0
+        high = len(A) - 1
+        while low < high:
+            mid = low + (high - low) // 2
+            if A[mid] < A[mid + 1]: low = mid + 1
+            elif A[mid] > A[mid + 1]: high = mid
+        return low
