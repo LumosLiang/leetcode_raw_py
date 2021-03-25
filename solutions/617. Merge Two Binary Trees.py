827926e3dc8517f0df4334352fa78a83ef53e7e4
lumos! <43442549+LumosLiang@users.noreply.github.com>
2021-03-25 23:02:34 +0800

    104 ms

--- solutions/617. Merge Two Binary Trees.py
+++ solutions/617. Merge Two Binary Trees.py
@@ -7,3 +7,15 @@
 class Solution:
     def mergeTrees(self, t1: TreeNode, t2: TreeNode) -> TreeNode:
         
+        # recursive
+     
+        if t1 is None and t2 is not None:
+            new_node = TreeNode(t2.val, t2.left, t2.right)
+            return new_node
+        elif t2 is None and t1 is not None:
+            new_node = TreeNode(t1.val, t1.left, t1.right)
+            return new_node
+        elif t1 and t2:
+            new_node = TreeNode(t1.val + t2.val, self.mergeTrees(t1.left, t2.left), self.mergeTrees(t1.right, t2.right))
+            return new_node
+     
