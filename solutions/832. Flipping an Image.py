5f8d9c49af8e79568395b9786ed6c65b048da07c
lumos! <43442549+LumosLiang@users.noreply.github.com>
2021-03-25 23:23:47 +0800

    32 ms

--- solutions/832. Flipping an Image.py
+++ solutions/832. Flipping an Image.py
@@ -1,3 +1,17 @@
-class Solution:
-    def flipAndInvertImage(self, A: List[List[int]]) -> List[List[int]]:
+class Solution(object):
+    def flipAndInvertImage(self, A):
+        """
+        :type A: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        # donot create more space in memory?
         
+        for i in range(len(A)):
+            # do horizontal job
+            A[i] = list(reversed(A[i]))
+            # do invert job, abs
+            
+            A[i] = [abs(j-1) for j in A[i]]
+            
+        return A
+    
