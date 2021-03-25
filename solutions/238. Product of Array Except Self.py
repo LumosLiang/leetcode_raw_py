a76363ce533afb066a08866e79906e14410e3b2b
lumos! <43442549+LumosLiang@users.noreply.github.com>
2021-03-25 23:53:47 +0800

    232 ms

---
+++ solutions/238. Product of Array Except Self.py
@@ -0,0 +1,16 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        
+        ans = [1] * len(nums)
+        
+        left, right = 1, 1
+        
+        for i in range(len(nums)):
+            ans[i] *= left
+            ans[-1 - i] *= right 
+            left *= nums[i]
+            right *= nums[-1 - i]
+        
+        return ans
+            
+        
