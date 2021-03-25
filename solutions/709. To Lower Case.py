17d199df6e141143c6102e9da1978574139d0ea6
lumos! <43442549+LumosLiang@users.noreply.github.com>
2021-03-25 23:09:49 +0800

    28 ms

--- solutions/709. To Lower Case.py
+++ solutions/709. To Lower Case.py
@@ -1,3 +1,10 @@
 class Solution:
     def toLowerCase(self, str: str) -> str:
+        res = ""
+        for char in str:
+            if ord(char) >= 65 and ord(char) <= 90:
+                res += chr(ord(char)+32)
+            else:
+                res += char
         
+        return res
