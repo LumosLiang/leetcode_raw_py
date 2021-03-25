4d7d370234e06a8f4cd186e7d984bb8eb25b70c8
lumos! <43442549+LumosLiang@users.noreply.github.com>
2021-03-25 23:20:40 +0800

    20 ms

--- solutions/804. Unique Morse Code Words.py
+++ solutions/804. Unique Morse Code Words.py
@@ -1,3 +1,21 @@
-class Solution:
-    def uniqueMorseRepresentations(self, words: List[str]) -> int:
+class Solution(object):
+    def uniqueMorseRepresentations(self, words):
+        """
+        :type words: List[str]
+        :rtype: int
+        """
+        
+        moss = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+        
+        # since all the letter is lower letter, we use ord()
+        theset = []
+        
+        for word in words:
+            str1 = ''
+            for letter in word:
+                index = ord(letter) - ord('a')
+                str1 += moss[index]
+            if str1 not in theset:    
+                theset.append(str1)
+        return len(theset)    
         
