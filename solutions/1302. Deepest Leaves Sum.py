c960b960d69cf1f9ac2bd4bbc8e527e80d4b609b
lumos! <43442549+LumosLiang@users.noreply.github.com>
2021-03-25 23:40:13 +0800

    84 ms

--- solutions/1302. Deepest Leaves Sum.py
+++ solutions/1302. Deepest Leaves Sum.py
@@ -9,6 +9,8 @@
 # method2: DFS, store the level and value, recursive
 ​
 class Solution:
-    def deepestLeavesSum(self, root: TreeNode) -> int:
-        
-        
+    def deepestLeavesSum(self, root):
+        q = [root]
+        while q:
+            pre, q = q, [child for p in q for child in [p.left, p.right] if child]
+        return sum(node.val for node in pre)
