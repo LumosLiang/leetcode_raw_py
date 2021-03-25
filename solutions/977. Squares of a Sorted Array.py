cf8c0d700f912a07dd4f9dcf724af7e3e643eefc
lumos! <43442549+LumosLiang@users.noreply.github.com>
2021-03-25 23:32:17 +0800

    184 ms

--- solutions/977. Squares of a Sorted Array.py
+++ solutions/977. Squares of a Sorted Array.py
@@ -1,3 +1,12 @@
-class Solution:
-    def sortedSquares(self, A: List[int]) -> List[int]:
-        
+import math
+class Solution(object):
+    def sortedSquares(self, A):
+        """
+        :type A: List[int]
+        :rtype: List[int]
+        """
+        lst = []
+        for a in A:
+            lst.append(int(math.pow(a,2)))
+        lst.sort()
+        return lst    
