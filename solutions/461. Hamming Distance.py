137147adfaaff2876ade813b3f23e4478bb7baf5
lumos! <43442549+LumosLiang@users.noreply.github.com>
2021-03-25 22:47:06 +0800

    Update 461. Hamming Distance.py

--- solutions/461. Hamming Distance.py
+++ solutions/461. Hamming Distance.py
@@ -1,3 +1,11 @@
-class Solution:
-    def hammingDistance(self, x: int, y: int) -> int:
-        
+class Solution(object):
+    def hammingDistance(self, x, y):
+        """
+        :type x: int
+        :type y: int
+        :rtype: int
+        """
+        # 异或运算 1的个数
+        
+        return list(bin(x ^ y)).count('1')
+        
