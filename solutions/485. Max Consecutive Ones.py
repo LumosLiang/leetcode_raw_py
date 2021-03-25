f28aa5d4afdd7fb5490957978abb0778725a68a7
lumos! <43442549+LumosLiang@users.noreply.github.com>
2021-03-25 22:48:20 +0800

    Update 485. Max Consecutive Ones.py

--- solutions/485. Max Consecutive Ones.py
+++ solutions/485. Max Consecutive Ones.py
@@ -1,3 +1,7 @@
-class Solution:
-    def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
-        
+class Solution(object):
+    def findMaxConsecutiveOnes(self, nums):
+        cnt=ans=0
+        for n in nums:
+            cnt=cnt+1 if n else 0
+            if cnt>ans: ans=cnt
+        return ans
