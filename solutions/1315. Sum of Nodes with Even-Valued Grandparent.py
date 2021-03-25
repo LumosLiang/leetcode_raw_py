76abb3faf098e14af80d7679ac80c18635d43005
lumos! <43442549+LumosLiang@users.noreply.github.com>
2021-03-25 23:42:21 +0800

    108 ms

--- solutions/1315. Sum of Nodes with Even-Valued Grandparent.py
+++ solutions/1315. Sum of Nodes with Even-Valued Grandparent.py
@@ -1,28 +1,24 @@
 class Solution:
-    
     def sumEvenGrandparent(self, root):
-        
-        res = 0
- 
-        def dfs(root, level):
-            nonlocal res
-            
-            tempLevel = level.copy()
-            for i in range(len(tempLevel)):
-                tempLevel[i] = tempLevel[i] - 1
-                if tempLevel[i] == 0:
-                    res += root.val
-​
-            if 0 in tempLevel:
-                tempLevel.remove(0)
-​
+        self.sum = 0
+        def dfs(root):
+            if not root: return 0
             if root.val % 2 == 0:
-                tempLevel.append(2)
-​
+                if root.left:
+                    if root.left.left:
+                        self.sum += root.left.left.val
+                    if root.left.right:
+                        self.sum += root.left.right.val
+                if root.right:
+                    if root.right.left:
+                        self.sum += root.right.left.val
+                    if root.right.right:
+                        self.sum += root.right.right.val
+            
             if root.left:
-                dfs(root.left, tempLevel)
+                dfs(root.left)
             if root.right:
-                dfs(root.right, tempLevel)
-            return res
-        
-        return dfs(root, [])
+                dfs(root.right)
+            
+        dfs(root)
+        return self.sum
