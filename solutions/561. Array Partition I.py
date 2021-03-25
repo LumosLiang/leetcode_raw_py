a81dfb83dfd097ff4213d393a534eefbb38ef66b
lumos! <43442549+LumosLiang@users.noreply.github.com>
2021-03-25 22:59:00 +0800

    80 ms

--- solutions/561. Array Partition I.py
+++ solutions/561. Array Partition I.py
@@ -1,3 +1,11 @@
-class Solution:
-    def arrayPairSum(self, nums: List[int]) -> int:
-        
+class Solution(object):
+    def arrayPairSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        R = 0
+        nums = sorted(nums)
+        for i in range(len(nums)/2):
+            R += nums[2*i]
+        return R
