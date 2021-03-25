1e5bfcd099d191f8993313653114dafe97238c64
lumos! <43442549+LumosLiang@users.noreply.github.com>
2021-03-25 23:05:43 +0800

    108 ms

--- solutions/657. Robot Return to Origin.py
+++ solutions/657. Robot Return to Origin.py
@@ -1,3 +1,26 @@
-class Solution:
-    def judgeCircle(self, moves: str) -> bool:
+class Solution(object):
+    def judgeCircle(self, moves):
+        """
+        :type moves: str
+        :rtype: bool
+        """
+        # left x - 1
+        # right x + 1
+        # up y + 1
+        # down y - 1
         
+        starting_pos = [0, 0]
+        
+        for s in moves:
+            if s == "U":
+                starting_pos[1] += 1
+            if s == "D":
+                starting_pos[1] -= 1
+            if s == "L":
+                starting_pos[0] -= 1
+            if s == "R":
+                starting_pos[0] += 1
+        if starting_pos == [0,0]:
+            return True
+        else:
+            return False
