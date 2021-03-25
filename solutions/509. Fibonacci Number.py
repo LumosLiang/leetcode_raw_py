248583d55c010208d4ecf658c488649d8c34743f
lumos! <43442549+LumosLiang@users.noreply.github.com>
2021-03-25 22:52:16 +0800

    Update 509. Fibonacci Number.py

--- solutions/509. Fibonacci Number.py
+++ solutions/509. Fibonacci Number.py
@@ -1,3 +1,8 @@
 class Solution:
-    def fib(self, N: int) -> int:
-        
+    def fib(self, N):
+        if N == 0:
+            return 0
+        elif N == 1:
+            return 1
+        else:
+            return self.fib(N - 1) + self.fib(N - 2)
