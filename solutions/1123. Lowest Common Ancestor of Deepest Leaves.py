7fbcb187caad8dc5ac1bf9bf0a7375171242115c
lumos! <43442549+LumosLiang@users.noreply.github.com>
2021-05-18 23:01:14 +0800

    36 ms

---
+++ solutions/1123. Lowest Common Ancestor of Deepest Leaves.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+​
+# recursion
+class Solution:
+    def lcaDeepestLeaves(self, root: TreeNode) -> TreeNode:
+        
+        def helper(root, d):
+            
+            if not root: return [None, d - 1]
+​
+            if not root.left and not root.right: return [root, d]
+​
+            [leftlca, ld] = helper(root.left, d + 1)
+            [rightlca, rd] = helper(root.right, d + 1)
+​
+            if ld > rd: return [leftlca, ld]
+            elif ld < rd:return [rightlca, rd]
+            else: return [root, ld]
+​
+        return helper(root, 0)[0]
