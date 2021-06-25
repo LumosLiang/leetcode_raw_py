5aa2976411cfbafbb1d097d08bac2f552fe52347
lumos! <43442549+LumosLiang@users.noreply.github.com>
2021-06-25 15:26:05 +0800

    N/A

---
+++ solutions/110. Balanced Binary Tree.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isBalanced(self, root: TreeNode) -> bool:
+        
+        def helper(root):
+            
+            if root is None: return [True, 0]
+            
+            isBL, LH = helper(root.left)
+            isBR, RH = helper(root.right)
+​
+            if abs(LH - RH) <= 1: return [True, max(LH, RH) + 1]
+            else: return [False, max(LH, RH) + 1]
+    
+        return helper(root)[0]
