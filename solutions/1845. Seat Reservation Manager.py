9aca951c2a02dc29dc3e8eb9aa841caa58f14c45
lumos! <43442549+LumosLiang@users.noreply.github.com>
2021-07-04 12:55:28 +0800

    592 ms

---
+++ solutions/1845. Seat Reservation Manager.py
@@ -0,0 +1,19 @@
+class SeatManager:
+​
+    def __init__(self, n: int):
+        
+        self.unresv = [i for i in range(1, n+1)]
+​
+    def reserve(self) -> int:
+        return heapq.heappop(self.unresv)
+        
+    def unreserve(self, seatNumber: int) -> None:
+        
+        heapq.heappush(self.unresv, seatNumber)
+        
+​
+​
+# Your SeatManager object will be instantiated and called as such:
+# obj = SeatManager(n)
+# param_1 = obj.reserve()
+# obj.unreserve(seatNumber)
