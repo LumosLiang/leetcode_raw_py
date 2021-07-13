244947d73ef64674754b2b9e116cc71cdc467988
lumos! <43442549+LumosLiang@users.noreply.github.com>
2021-07-13 21:14:21 +0800

    N/A

---
+++ solutions/40. Combination Sum II.py
@@ -0,0 +1,18 @@
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        res = []
+        candidates.sort()
+        
+        def helper(choices, path, s):
+            if s <= 0:
+                if s == 0:
+                    res.append(path)
+                    return
+            
+            for i in range(len(choices)):
+                if i > 0 and choices[i] == choices[i - 1]: continue
+                helper(choices[i + 1:], path + [choices[i]], s - choices[i])
+            
+        helper(candidates, [], target)
+        return res
+        
