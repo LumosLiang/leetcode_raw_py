fd080d454cb5570602bcb2a743f73ea495f54b44
lumos! <43442549+LumosLiang@users.noreply.github.com>
2021-07-21 00:00:23 +0800

    1428 ms

--- solutions/322. Coin Change.py
+++ solutions/322. Coin Change.py
@@ -1,18 +1,13 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        memo = {0:0, -1:float('inf')}
         
-        def dp(i):
-            if i not in memo:
-                res = float('inf')
-                for coin in coins:
-                    subproblem = dp(i - coin)
-                    res = min(res, 1 + subproblem)
-                memo[i] = res if res != float('inf') else -1
-            return memo[i]
+        dp = (amount + 1) * [amount + 1]
         
+        dp[0] = 0
         
-        
-        
-        return dp(amount)
+        for i in range(len(dp)):
+            for coin in coins:
+                if i - coin < 0: continue
+                dp[i] = min(dp[i], 1 + dp[i - coin])
 ​
+        return dp[amount] if dp[amount] != amount + 1 else -1
