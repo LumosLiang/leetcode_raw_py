fd8f2cd38fddf174859e03e4d68ac6644124a32c
lumos! <43442549+LumosLiang@users.noreply.github.com>
2021-09-13 17:39:46 +0800

    197 ms

---
+++ solutions/12. Integer to Roman.py
@@ -0,0 +1,13 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        
+        res = ""
+        
+        d = {1000: 'M', 900: 'CM', 500: 'D', 400: 'CD', 100: 'C', 90: 'XC', 50: 'L', 40: 'XL', 10: 'X', 9: 'IX', 5: 'V', 4: 'IV', 1: 'I'}
+        
+        for key in d:
+            while num >= key:
+                res += d[key] 
+                num -= key
+        
+        return res
