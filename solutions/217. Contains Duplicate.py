6c67f2ee56866519b64e804573a99fb40e79ad69
lumos! <43442549+LumosLiang@users.noreply.github.com>
2021-10-20 14:43:08 +0800

    169 ms

--- solutions/217. Contains Duplicate.py
+++ solutions/217. Contains Duplicate.py
@@ -1,3 +1,11 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         
+        hash = collections.Counter(nums)
+        
+        for val in list(hash.values()):
+            if val != 1: 
+                return True
+                break
+        
+        return False
