682c10ebd3634e9685bc11415eabf6a5c3c2134f
lumos! <43442549+LumosLiang@users.noreply.github.com>
2021-10-25 11:39:53 +0800

    32 ms

--- solutions/21. Merge Two Sorted Lists.py
+++ solutions/21. Merge Two Sorted Lists.py
@@ -4,5 +4,24 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+    def mergeTwoLists(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         
+        curr1, curr2 = l1, l2
+        dummy = ListNode()
+        curr = dummy
+        
+        if not l1 or not l2: return l1 or l2
+        
+        while curr1 and curr2:
+            if curr1.val >= curr2.val:
+                curr.next = curr2
+                curr2 = curr2.next
+            else:
+                curr.next = curr1
+                curr1 = curr1.next
+            curr = curr.next
+            
+        if curr1: curr.next = curr1
+        else: curr.next = curr2
+            
+        return dummy.next
