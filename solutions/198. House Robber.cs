376c752b73420efdbf8f49bbc80dbbe7560fb974
lumos! <43442549+LumosLiang@users.noreply.github.com>
2021-10-25 17:21:08 +0800

    132 ms

---
+++ solutions/198. House Robber.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public int Rob(int[] nums)
+    {
+        if (nums.Length == 1) return nums[0];
+        
+        int[] dp = new int[nums.Length];
+        dp[0] = nums[0];
+        dp[1] = Math.Max(dp[0], nums[1]);
+​
+        for (int i = 2; i < nums.Length; i++)
+        {
+            dp[i] = Math.Max(nums[i] + dp[i - 2], dp[i - 1]);
+        }
+​
+        return dp[nums.Length - 1];
+    }
+}
