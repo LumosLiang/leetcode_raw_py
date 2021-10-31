1889c21af861b327c6f38cd3fa952712c251c96a
lumos! <43442549+LumosLiang@users.noreply.github.com>
2021-10-31 23:27:10 +0800

    56 ms

--- solutions/784. Letter Case Permutation.py
+++ solutions/784. Letter Case Permutation.py
@@ -5,17 +5,17 @@ class Solution:
         
         def backtrack(lst, path, idx):
             
-            if(len(path) ==  len(lst)): 
-                res.append(''.join(path))
+            if(idx == len(lst)): 
+                res.append(path)
             else:
                 c = lst[idx]
                 if str.isalpha(c):
-                    backtrack(lst, path + [c.lower()], idx + 1)
-                    backtrack(lst, path + [c.upper()], idx + 1)
+                    backtrack(lst, path + c.lower(), idx + 1)
+                    backtrack(lst, path + c.upper(), idx + 1)
                 else:
-                    backtrack(lst, path + [c], idx + 1)
+                    backtrack(lst, path + c, idx + 1)
             
-        backtrack(list(s), [], 0)    
+        backtrack(list(s), '', 0)    
         
         return res
         
