b25b819b59e5d3d56391671bcb1db61064e07f57
“LumosLiang” <lsllywjb@gmail.com>
2021-11-05 19:51:03 +0800

    Change format

--- solutions/213. House Robber II.py
+++ solutions/213. House Robber II.py
@@ -1,18 +1,15 @@
 class Solution:
-    def rob(self, nums: List[int]) -> int:
-        l = len(nums)
-        if l == 1: return nums[0]        
-        return max(self.rob_helper(nums[:l - 1]), self.rob_helper(nums[1:]))
-        
-        
-    def rob_helper(self, nums):
-         
-        pre, prepre, curr = 0, 0, 0
-        
-        for i in range(len(nums)):
-            curr = max(pre, prepre + nums[i])
-            prepre = pre
-            pre = curr
-        
-        return curr
-        
+    def rob(self, nums: List[int]) -> int:
+        l = len(nums)
+        if l == 1: return nums[0]
+
+        return max(self.rob_helper(nums[:l - 1]), self.rob_helper(nums[1:]))
+    def rob_helper(self, nums):
+
+        pre, prepre, curr = 0, 0, 0
+        for i in range(len(nums)):
+            curr = max(pre, prepre + nums[i])
+            prepre = pre
+            pre = curr
+            
+        return curr
