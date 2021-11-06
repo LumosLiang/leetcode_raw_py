db3aade74ebf116aae00385fb5c1afbdefca2e54
lumos! <43442549+LumosLiang@users.noreply.github.com>
2021-11-06 16:01:25 +0800

    68 ms

---
+++ solutions/120. Triangle.py
@@ -0,0 +1,20 @@
+class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        
+        init = triangle[0][0]
+        
+        l = len(triangle)
+        
+        if l == 1: return init
+        
+        dp = [init]
+        
+        for i in range(1, l):
+            temp = []
+            for j in range(len(triangle[i])):
+                if j == 0: temp.append(dp[0] + triangle[i][j])
+                elif j == len(triangle[i]) - 1: temp.append(dp[j - 1] + triangle[i][j])
+                else: temp.append(min(dp[j - 1], dp[j]) + triangle[i][j])
+            dp = temp
+            
+        return min(dp)
