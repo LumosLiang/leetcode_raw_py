a3acac8bcc8e2e85ede4ec3cc0c71c9be6cf934a
lumos! <43442549+LumosLiang@users.noreply.github.com>
2021-11-13 23:01:26 +0800

    851 ms

--- solutions/219. Contains Duplicate II.py
+++ solutions/219. Contains Duplicate II.py
@@ -1,14 +1,13 @@
 class Solution:
-    def containsNearbyDuplicate(self, nums, k):
-        value_index = {}
-        for idx, value in enumerate(nums):
-            if value not in value_index:
-                value_index[value] = [idx]
-            else:
-                value_index[value].append(idx)
-                if len(value_index[value]) >= 2 and (value_index[value][-1] - value_index[value][-2] <= k):
-                    return True
-                else:
-                    continue
-
-        return False
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        
+        window = {}
+        
+        for r, val in enumerate(nums):
+            
+            if val in window and r - window[val] <= k:
+                return True
+                
+            window[val] = r
+                
+        return False
