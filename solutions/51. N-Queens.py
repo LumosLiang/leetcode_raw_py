2c0f4a6d73436d34912a733ec2b5fd1ee968eb19
lumos! <43442549+LumosLiang@users.noreply.github.com>
2021-11-18 16:08:00 +0800

    1148 ms

---
+++ solutions/51. N-Queens.py
@@ -0,0 +1,33 @@
+class Solution:
+    def solveNQueens(self, n: int):
+        res, choices = [], []
+        
+        for i in range(n):
+            for j in range(n):
+                choices.append((i,j))
+                
+        board = [["" for _ in range(n)] for _ in range(n)]
+        
+        def backtrack(choices, board, queencnt, cnt):
+            if not choices and queencnt == cnt:
+                temp = ["".join(b) for b in board]
+                res.append(temp)
+                return
+            
+            for i in range(len(choices)):
+                m, n = choices[i]
+                board[m][n] = "Q"
+                
+                new_choices = []
+                for item in choices[i + 1:]:
+                    x, y = item[0], item[1]
+                    if x == m or y == n or (abs(x - m) == abs(y - n)):
+                        board[x][y] = "."
+                    else:
+                        new_choices.append((x, y))
+​
+                backtrack(new_choices, board, queencnt + 1, cnt)
+                board[m][n] = "."
+                
+        backtrack(choices, board, 0, n)
+        return res
