05f0e54b7ffc6c49ba14f722d86c19f66854f883
lumos! <43442549+LumosLiang@users.noreply.github.com>
2021-11-22 18:49:03 +0800

    54 ms

--- solutions/513. Find Bottom Left Tree Value.py
+++ solutions/513. Find Bottom Left Tree Value.py
@@ -1,5 +1,25 @@
 class Solution:
     def findBottomLeftValue(self, root: 'TreeNode') -> 'int':
+        
+        self.res = 0
+        self.pre_height = 0
+        
+        def helper(root, curr_height):
+            if not root: return
+            
+            if curr_height > self.pre_height:
+                self.res = root.val
+                self.pre_height = curr_height
+                
+            helper(root.left, curr_height + 1)
+            helper(root.right, curr_height + 1)
+        
+        helper(root, 1)
+  
+        return self.res 
+​
+class Solution:
+    def findBottomLeftValue(self, root: 'TreeNode') -> 'int':
         queue = [root]
         while queue:
             node = queue.pop(0)
@@ -9,3 +29,4 @@ class Solution:
             if node.left:
                 queue.append(node.left)
         return res
+​
