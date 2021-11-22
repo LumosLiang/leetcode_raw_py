db2b3d063f9972d7fa2fe9c707908624ca5fec9e
lumos! <43442549+LumosLiang@users.noreply.github.com>
2021-11-22 12:10:37 +0800

    55 ms

--- solutions/100. Same Tree.py
+++ solutions/100. Same Tree.py
@@ -4,13 +4,42 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+​
 class Solution:
-    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
-        if p is None and q is None: return True
-        
-        if p is None or q is None: return False
-    
+    def isSameTreeRecur(self, p:TreeNode, q:TreeNode):
+        if not p and not q: return True
+​
+        if not p or not q: return False
+​
         if p.val != q.val: return False
+​
+        return self.isSameTreeRecur(p.left, q.left) and self.isSameTreeRecur(p.right, q.right)
+​
+    def isSameTreeBFS(self, p:TreeNode, q:TreeNode):
         
-        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
-        
+        q = collections.deque([[p,q]])
+​
+        while q:
+​
+            n1, n2 = q.popleft()
+            if not n1 and not n2: continue
+            if not n1 or not n2: return False
+            if n1.val != n2.val: return False
+            q.append([n1.left, n2.left])
+            q.append([n1.right, n2.right])
+​
+        return True
+​
+    def isSameTreeDFS(self, p:TreeNode, q:TreeNode):
+​
+        stack = [(p, q)]
+​
+        while stack:
+            t1, t2 = stack.pop()
+            if not t1 and not t2: return True
+            if not t1 or not t2: return False
+            if t1.val != t2.val: return False
+            stack.append((t1.right,t2.right))
+            stack.append((t1.left,t2.left))
+​
+        return True
