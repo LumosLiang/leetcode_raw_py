68d3232c2f55ab68ee7e2c497dea1c0b155508f9
lumos! <43442549+LumosLiang@users.noreply.github.com>
2021-11-25 23:53:18 +0800

    32 ms

--- solutions/404. Sum of Left Leaves.py
+++ solutions/404. Sum of Left Leaves.py
@@ -7,23 +7,42 @@
 ​
 # recursive
 ​
+# class Solution:
+#     def sumOfLeftLeaves(self, root: TreeNode) -> int:
+        
+#         if not root: return 0
+        
+#         if root.left and not root.left.left and not root.left.right: 
+#             return root.left.val + self.sumOfLeftLeaves(root.right)
+        
+#         return self.sumOfLeftLeaves(root.left) + self.sumOfLeftLeaves(root.right)
+​
+# BFS    
+    
+# class Solution:
+#     def sumOfLeftLeaves(self, root: TreeNode) -> int:
+        
+#         res, q =  0, collections.deque([root])
+        
+#         while q:
+#             node = q.popleft()
+#             if not node: continue
+#             if node.left and not node.left.left and not node.left.right: res += node.left.val
+#             if node.right: q.append(node.right)
+#             if node.left: q.append(node.left)
+#         return res
+        
+​
+# DFS
 class Solution:
     def sumOfLeftLeaves(self, root: TreeNode) -> int:
         
-        def DFS(node, isleft = False):
-            
-            if node is None: return 0
-            
-            if node.left and node.right:  
-                return DFS(node.left, True) + DFS(node.right, False)
-            if not node.left and not node.right:
-                return node.val if isleft else 0          
-            if not node.left and node.right:
-                return DFS(node.right, False)
-            if not node.right and node.left:
-                return DFS(node.left, True)
-            
-        return DFS(root)
+        res, stack = 0, [root]
         
-        
-        
+        while stack:
+            node = stack.pop()
+            if not node: continue
+            if node.left and not node.left.left and not node.left.right: res += node.left.val
+            if node.right: stack.append(node.right)
+            if node.left: stack.append(node.left)
+        return res
