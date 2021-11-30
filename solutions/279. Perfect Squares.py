310b5e16a4400b986caf69bb55355adaedbafb17
lumos! <43442549+LumosLiang@users.noreply.github.com>
2021-11-30 23:40:02 +0800

    6780 ms

--- solutions/279. Perfect Squares.py
+++ solutions/279. Perfect Squares.py
@@ -1,21 +1,17 @@
 class Solution:
     def numSquares(self, n: int) -> int:
         
-        while( n % 4 == 0 ):
-            # Reduction by factor of 4
-            n //= 4
-            
-        if n % 8 == 7:
-            # Quick response for n = 8k + 7
-            return 4
+        dp = (n + 1) * [0]
+        dp[0] = 0
         
-        # Check whether n = a^2 + b^2
-        for a in range( int(sqrt(n))+1 ):
+        for i in range(1, n + 1):
+            temp_min = float('inf')
+            maxps = math.floor(math.sqrt(i))
             
-            b = int( sqrt( n - a*a ) )
+            for j in range(1, maxps + 1):
+                temp_min = min(temp_min, dp[i - j ** 2])
             
-            if ( a**2 + b ** 2 ) == n :
-                return (a>0) + (b>0)
+            dp[i] = 1 + temp_min
             
-        # n = a^2 + b^2 + c^2
-        return 3
+        
+        return dp[n]
