82ef816b6214be044c83f9d189b6eb07e24e4831
lumos! <43442549+LumosLiang@users.noreply.github.com>
2021-12-02 18:46:46 +0800

    32 ms

--- solutions/783. Minimum Distance Between BST Nodes.py
+++ solutions/783. Minimum Distance Between BST Nodes.py
@@ -8,11 +8,33 @@
 class Solution:
     def minDiffInBST(self, root: TreeNode) -> int:
       
-        lst = []
-        def inorder(root):
-            if root.left: inorder(root.left)
-            lst.append(root.val)
-            if root.right: inorder(root.right)
-       
-        inorder(root)
-        return min(b - a for a, b in zip(lst, lst[1:])) 
+        # self.res, self.pre = float('inf'), float('-inf')
+        # self.solution1(root)
+        # return self.res
+    
+        return self.solution2(root, float('inf'), float('-inf'))
+    
+    def solution1(self, root):
+        
+        if not root: return
+        
+        self.solution1(root.left)
+        self.res = min(self.res, root.val - self.pre)
+        self.pre = root.val
+        self.solution1(root.right)
+        
+    
+    def solution2(self, root, high, low):
+        
+        if not root: return high - low
+        left = self.solution2(root.left, root.val, low)
+        right = self.solution2(root.right, high, root.val)
+        return min(left, right)
+​
+        
+        
+        
+        
+        
+        
+        
