7c99bb50aea42cc6d2dd1c5a6a3a65227b996124
lumos! <43442549+LumosLiang@users.noreply.github.com>
2021-12-14 21:21:37 +0800

    48 ms

--- solutions/22. Generate Parentheses.py
+++ solutions/22. Generate Parentheses.py
@@ -1,14 +1,35 @@
 class Solution:
-    def generateParenthesis(self, n: int) -> List[str]:
+    def generateParenthesis1(self, n: int) -> List[str]:
+        
+        res = []
         
         def backtrack(left, right, path):
-            if left > right or left < 0 or right < 0:
+            if left < 0 or right < 0 or right < left:
                 return
+            
             if left == right == 0:
-                self.res.append(path)
-            backtrack(left - 1, right, path + '(')
-            backtrack(left, right - 1, path + ')')
+                res.append(path)
+            
+            backtrack(left - 1, right, path + "(")
+            backtrack(left, right - 1, path + ")")
         
-        self.res = []
-        backtrack(n, n, '')
-        return self.res
+        backtrack(n, n, "")
+        return res
+    
+    
+    def generateParenthesis(self, n: int) -> List[str]:
+        
+        dp = [[""]]
+        
+        for i in range(1, n + 1):
+            temp = []
+            for j in range(i):
+                # (f(i))f(j)  0 <= j <= i - 1
+                for x in dp[j]:
+                    for y in dp[-j-1]:
+                        temp.append(f"({x}){y}")
+            
+            dp.append(temp)
+        return dp[-1]
+    
+​
