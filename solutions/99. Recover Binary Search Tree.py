8f34c7af62f4b0387b5358f5b0d8e7d4b86b211d
lumos! <43442549+LumosLiang@users.noreply.github.com>
2021-12-14 19:17:07 +0800

    72 ms

--- solutions/99. Recover Binary Search Tree.py
+++ solutions/99. Recover Binary Search Tree.py
@@ -9,25 +9,9 @@ class Solution:
         """
         Do not return anything, modify root in-place instead.
         """
-        
-        # Two node were swapped here. 
-        
-        # Think about this:
-        # If we do the in-order traversal and iterate the array from left to right, at some place, the current node will be less than the prev node. And this could happen at most twice. 
-            # If it happen once: The two adjacent nodes: prev and curr are exactly the ones we want
-            
-            1 -> 2 -> 4 -> 3 -> 5 -> 6
-            
-            # If it happen twice: Then at the first time, the prev node is the one we want; at the second time, the curr node is what we want.
-            
-            1 -> 5 -> 3 -> 4 -> 2 -> 6
-            
-        # So our job is to find that two nodes: node1, node2 by using in-order DFS. In fact, we actually do not need to care if this the first time or the second time, let's see these:
-            # when curr node's val is larger than prev node's, we continue and update the prev
-            # when curr node's val is smaller than prev node's, we do not need to update the prev, but just keep update node1 and node2 together. This is because, the val of nodes between node 1 and node2, include node2, must smaller than node1's. So once we find node1, we can safely do the update until node2.
-            
-   
+      
         self.node1, self.node2, self.pre = None, None, TreeNode(float('-inf'))
+        self.findFirst = False
         
         def helper(root):
             
@@ -36,10 +20,13 @@ class Solution:
             helper(root.left)
             
             if root.val < self.pre.val:
-                self.node1 = self.pre
+                if not self.findFirst:
+                    self.node1 = self.pre
+                    self.findFirst = True
+                
                 self.node2 = root
-            else:
-                self.pre = root
+            
+            self.pre = root
             
             helper(root.right)
         
