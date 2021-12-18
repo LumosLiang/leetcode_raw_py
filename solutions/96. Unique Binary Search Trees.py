91ada962ff509279d637579779cb98018f0fc332
lumos! <43442549+LumosLiang@users.noreply.github.com>
2021-12-18 13:34:03 +0800

    24 ms

--- solutions/96. Unique Binary Search Trees.py
+++ solutions/96. Unique Binary Search Trees.py
@@ -1,33 +1,14 @@
 class Solution:
     def numTrees(self, n: int) -> int:
-        lst = [i for i in range(1, n + 1)]
-        dit = collections.defaultdict(int)
         
-        def numOfTrees(lst):
-            # give you a inorder list
-            # give me how many BSTs it can generate
-            
-            if not lst: return 1
-            
-            if not dit[str(lst)]:
-                res = 0
-                for i in range(len(lst)):
-                    res += numOfTrees(lst[:i]) * numOfTrees(lst[i + 1:])  
-                dit[str(lst)] = res
-                return res
-            
-            return dit[str(lst)]
-​
-        return numOfTrees(lst)
-                
-            
+        # bottom up recursion
         
+        dp = [0] * (n + 1)
+        dp[0] = 1
+        dp[1] = 1
         
-            
-            
+        for i in range(2, n + 1):
+            for j in range(0, i):
+                dp[i] += dp[j] * dp[i - j - 1]
         
-        
-        
-        
-        
-        
+        return dp[n]
