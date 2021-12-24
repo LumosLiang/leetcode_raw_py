c2c043f727b174d0ffcc78d104650d3f66111bc8
lumos! <43442549+LumosLiang@users.noreply.github.com>
2021-12-24 16:20:58 +0800

    316 ms

--- solutions/200. Number of Islands.py
+++ solutions/200. Number of Islands.py
@@ -1,5 +1,5 @@
 class Solution:
-    def numIslands(self, grid: List[List[str]]) -> int:
+    def numIslandsBFS(self, grid: List[List[str]]) -> int:
         
         res = 0
         m = len(grid)
@@ -22,7 +22,7 @@ class Solution:
                 
         return res
 ​
-    def numIslandsTLE(self, grid: List[List[str]]) -> int:
+    def numIslandsBFSTLE(self, grid: List[List[str]]) -> int:
         
         res = 0
         m = len(grid)
@@ -39,9 +39,35 @@ class Solution:
                         grid[x][y] = '0'
                         for l, k in [x, y + 1],[x, y - 1],[x - 1, y],[x + 1, y]:
                             if 0 <= l <= m - 1 and 0 <= k <= n - 1 and grid[l][k] == '1':
-                                
                                 q.append([l, k])
                 
         return res
     
-​
+   
+    def numIslandsDFS1(self, grid: List[List[str]]) -> int:
+        
+        res = 0
+        m = len(grid)
+        n = len(grid[0])
+    
+        def DFS(x, y):
+            for l, k in [x, y + 1],[x, y - 1],[x - 1, y],[x + 1, y]:
+                if 0 <= l <= m - 1 and 0 <= k <= n - 1 and grid[l][k] == '1':
+                    grid[l][k] = '0'
+                    DFS(l, k)
+        
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == '1':
+                    res += 1
+                    
+                    grid[i][j] = '0'
+                    DFS(i, j)
+                    
+        return res
+    
+    def numIslandsDFS2(self, grid: List[List[str]]) -> int:
+        
+        res = 0
+        m = len(grid)
+        n = len(grid[0])
