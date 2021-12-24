568434b619dfb7df437c484db880adc155d4ec19
lumos! <43442549+LumosLiang@users.noreply.github.com>
2021-12-24 15:39:49 +0800

    N/A

--- solutions/200. Number of Islands.py
+++ solutions/200. Number of Islands.py
@@ -11,11 +11,37 @@ class Solution:
                     res += 1
                     
                     q = collections.deque([[i,j]])
+                    grid[i][j] = '0'
+                    
                     while q:
-                        x,y = q.popleft()
-                        grid[x][y] = '0'
+                        x,y = q.popleft()    
                         for l, k in [x, y + 1],[x, y - 1],[x - 1, y],[x + 1, y]:
                             if 0 <= l <= m - 1 and 0 <= k <= n - 1 and grid[l][k] == '1':
+                                grid[l][k] = '0'
                                 q.append([l, k])
                 
         return res
+​
+    def numIslandsTLE(self, grid: List[List[str]]) -> int:
+        
+        res = 0
+        m = len(grid)
+        n = len(grid[0])
+        
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == '1':
+                    res += 1
+                    
+                    q = collections.deque([[i,j]])
+                    while q:
+                        x,y = q.popleft()    
+                        grid[x][y] = '0'
+                        for l, k in [x, y + 1],[x, y - 1],[x - 1, y],[x + 1, y]:
+                            if 0 <= l <= m - 1 and 0 <= k <= n - 1 and grid[l][k] == '1':
+                                
+                                q.append([l, k])
+                
+        return res
+    
+​
