254aca9c0aacc6d86996a5606512785d6f6204c1
lumos! <43442549+LumosLiang@users.noreply.github.com>
2021-12-26 21:08:04 +0800

    304 ms

--- solutions/547. Number of Provinces.py
+++ solutions/547. Number of Provinces.py
@@ -1,5 +1,5 @@
 class Solution:
-    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+    def findCircleNum1(self, isConnected: List[List[int]]) -> int:
         
         # DFS & Backtrack
         
@@ -23,6 +23,35 @@ class Solution:
                             if isConnected[x][ny] == 1:
                                 isConnected[x][ny] = 0
                                 if x != ny:
+                                    q.append((ny, x))
+        
+        return cnt
+​
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        
+        # BFS
+        l, w = len(isConnected[0]), len(isConnected)
+        
+        cnt = 0
+        
+        for i in range(l):
+            for j in range(w):
+                if isConnected[i][j] == 1:
+                    
+                    q = collections.deque([(i, j)])
+                    cnt += 1
+                    
+                    isConnected[i][j] = 0
+                    
+                    while q:
+                        x, y = q.popleft()
+                        
+                        for ny in range(0, l):
+                            if ny == y: continue
+                            if isConnected[x][ny] == 1:
+                                isConnected[x][ny] = 0
+                                if x != ny:
+                                    isConnected[ny][x] = 0
                                     q.append((ny, x))
         
         return cnt
