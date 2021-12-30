209189bd58f1c83e61c44c1a550e591837750174
lumos! <43442549+LumosLiang@users.noreply.github.com>
2021-12-30 15:20:56 +0800

    59 ms

--- solutions/225. Implement Stack using Queues.py
+++ solutions/225. Implement Stack using Queues.py
@@ -1,44 +1,60 @@
 from collections import deque
 ​
+# if using deque, I should only use deque: append, and popleft
+​
 class MyStack:
 ​
     def __init__(self):
         """
         Initialize your data structure here.
         """
-        self.q1 = deque()
-        self.q2 = deque()
-        self._top = None
-        
+        self.q = deque()
 ​
     def push(self, x: int) -> None:
         """
         Push element x onto stack.
         """
-        self.q1.append(x)
-        self._top = x
+        tempq = deque()
+        tempq.append(x)
+        tempq.append(self.q)
+        self.q = tempq
         
-​
     def pop(self) -> int:
         """
         Removes the element on top of the stack and returns that element.
         """
-        while len(self.q1) > 1:
-            self._top = self.q1.popleft()
-            self.q2.append(self._top)
-        result = self.q1.popleft()
-        self.q1, self.q2 = self.q2, self.q1
-        return result
-        
-​
+        res = self.q.popleft()
+        self.q = self.q.popleft()
+        return res
+    
     def top(self) -> int:
         """
         Get the top element.
         """
-        return self._top
+        return self.q[0]
+        
 ​
     def empty(self) -> bool:
         """
         Returns whether the stack is empty.
         """
-        return len(self.q1) == 0
+        return len(self.q) == 0 
+    
+​
+    
+# I think this is the incorect one 
+​
+# from collections import deque
+​
+# class MyStack:
+​
+#     def __init__(self):
+#         """
+#         Initialize your data structure here.
+#         """
+#         self.q1 = deque()
+#         self.q2 = deque()
+#         self._top = None
+        
+​
+#     def push(self, x: int) -> None:
