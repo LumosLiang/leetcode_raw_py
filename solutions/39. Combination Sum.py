3b3f3804048ca768bb8f808757fcd907321884b9
lumos! <43442549+LumosLiang@users.noreply.github.com>
2022-01-06 22:24:23 +0800

    72 ms

--- solutions/39. Combination Sum.py
+++ solutions/39. Combination Sum.py
@@ -1,17 +1,37 @@
+# Trik is excluding duplicate zat 
+# choices[i:]
+​
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         res = []
         
-        def helper(choices, path, s):
+        def helper(choices, start, path, s):
             if s == 0:
                 res.append(path)
                 return
             elif s < 0:
                 return
             
-            for i in range(len(choices)):
-                helper(choices[i:], path + [choices[i]], s - choices[i])
+            for i in range(start, len(choices)):
+                helper(choices, i, path + [choices[i]], s - choices[i])
             
-        helper(candidates, [], target)
+        helper(candidates, 0, [], target)
+        return res
+        
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        res = []
+        
+        def helper(choices, path):
+            if sum(path) == target:
+                res.append(path)
+                return
+            elif sum(path) > target:
+                return
+            
+            for i in range(len(choices)):
+                helper(choices[i:], path + [choices[i]])
+            
+        helper(candidates, [])
         return res
         
