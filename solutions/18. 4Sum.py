a57d96483ccb141150d7a1dc30adb0e8334f1a0d
lumos! <43442549+LumosLiang@users.noreply.github.com>
2022-02-15 16:24:39 +0800

    1115 ms

--- solutions/18. 4Sum.py
+++ solutions/18. 4Sum.py
@@ -1,30 +1,56 @@
+​
 class Solution:
-    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
-
-        nums.sort()
-        res = []
-        for i in range(len(nums) - 3):
-            if i > 0 and nums[i] == nums[i - 1]:
-                continue
-            for j in range(i + 1, len(nums) - 2):
-                if j > i + 1 and nums[j] == nums[j - 1]:
-                    continue
-                t = target - nums[i] - nums[j]
-                l, r = j + 1, len(nums) - 1
-                while l < r:
-                    s = nums[l] + nums[r]
-                    if s == t:
-                        res.append([nums[i], nums[j], nums[l], nums[r]])
-                        while l < r and nums[l] == nums[l + 1]:
-                         l += 1
-                        while l < r and nums[r] == nums[r - 1]:
-                         r -= 1
-                        r -= 1
-                        l += 1
-                    elif s > t:
-                        r -= 1
-                    else:
-                        l += 1
-        return res
-                        
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        
+        # return self.solution1(nums, target)
+        return self.solution2(nums, target)
+    
+    # TLE int
+    def solution1(self, nums, target):↔​
+        
+    # two pointers 
+        # not sorted - two sum - hashmap - O(N)
+        # sort - two sum - two pointer - O(NlogN) + O(N)
+        # sort - three sum -  O((N - 2) * N)
+        # sort - four sum - O((N - 3) * ((N - 2) * N))
+    
+    def solution2(self, nums, target):
+        if len(nums) < 4: return None
+        
+        nums.sort()
+        res, l = [], len(nums)
+        
+        for i in range(l - 3):
+            if i > 0 and nums[i] == nums[i - 1]: continue
+            target_three = target - nums[i]
+            
+            for j in range(i + 1, l - 2):
+                if j > i + 1 and nums[j] == nums[j - 1]: continue
+                target_two = target_three - nums[j]
                 
+                p1, p2 = j + 1, l - 1
+                
+                while p1 < p2:
+                    if nums[p1] + nums[p2] == target_two:
+                        res.append([nums[i], nums[j], nums[p1], nums[p2]])
+                        while p1 < p2 and nums[p1 + 1] == nums[p1]: p1 += 1
+                        while p1 < p2 and nums[p2 - 1] == nums[p2]: p2 -= 1
+                        p1 += 1
+                        p2 -= 1
+                    elif nums[p1] + nums[p2] > target_two:
+                        p2 -= 1
+                    else:
+                        p1 += 1
+        
+        return res
+                
+                
+                
+                
+                
+                
+            
+        
+        
+        
+        
