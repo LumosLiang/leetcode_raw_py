988c4604fed23a0818f8d3addf1e3e111998441b
lumos! <43442549+LumosLiang@users.noreply.github.com>
2022-03-02 22:55:28 +0800

    336 ms

--- solutions/189. Rotate Array.py
+++ solutions/189. Rotate Array.py
@@ -1,10 +1,43 @@
 class Solution:
-    def rotate(self, nums, k):
-        """
-        Do not return anything, modify nums in-place instead.
-        """
-        while k != 0:
-            nums.insert(0, nums[-1])
-            nums.pop(-1)
-            k -= 1
-        
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        self.solution3(nums, k)
+    
+    # in place O(KN), O(1)
+    def solution1(self, nums, k):
+        cnt = 0
+        
+        while cnt < k:
+            item = nums.pop()
+            nums.insert(0,item)   
+            cnt += 1
+    
+    # hash O(N), O(N)
+    def solution2(self, nums, k):
+        nums_copy, l = copy.deepcopy(nums), len(nums)
+        for i in range(len(nums_copy)):
+            nums[(i + k) % l] = nums_copy[i]
+   
+    # two pointers O(N), O(1)
+    def solution3(self, nums, k):
+        
+        def helper(nums, start, end):
+            
+            if start >= end or start < 0 or end > len(nums) - 1: return
+            
+            while start < end:
+                nums[start], nums[end] = nums[end], nums[start]
+                start += 1
+                end -= 1
+        
+        k = k % len(nums)
+        helper(nums, 0, len(nums) - 1)
+        helper(nums, 0, k - 1)
+        helper(nums, k, len(nums) - 1)
+        
+            
+            
+        
+    
