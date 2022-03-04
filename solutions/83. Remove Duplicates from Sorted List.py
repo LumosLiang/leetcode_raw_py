4ae3053b0f68bf6cab01e591b4e4dd5585048ad4
lumos! <43442549+LumosLiang@users.noreply.github.com>
2022-03-04 21:58:38 +0800

    38 ms

--- solutions/83. Remove Duplicates from Sorted List.py
+++ solutions/83. Remove Duplicates from Sorted List.py
@@ -3,18 +3,57 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+​
 class Solution:
-    def deleteDuplicates(self, head: ListNode) -> ListNode:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
         
-        if head is None:return None
+        # This can be acheived in-place or not in-place
+        return self.inplace(head)
+    
+    # O(N), O(1)
+    def inplace(self, head):
         
-        slow = fast = head
+        dummyN = ListNode(-101)
+        dummyN.next = head
         
-        while fast:
-            if fast.val != slow.val:
-                slow.next = fast
-                slow = slow.next
-            fast = fast.next
-        slow.next = None
-        return head
+        pre, curr, nxt = dummyN, head, None
+        
+        while curr:
+            nxt = curr.next
+            if curr.val == pre.val:
+                pre.next = nxt
+            else:
+                pre = curr
+            curr = nxt
+        
+        return dummyN.next
+        
+    # O(N), O(N)
+    def notinplace(self, head):
+        
+        curr, lst = head, []
+        
+        while curr:
+            lst.append(curr.val)
+            curr = curr.next
+        
+        l, r = 0, 0
+        
+        while r < len(lst):
+            if lst[r] != lst[l]:
+                lst[l + 1] = lst[r]
+                l += 1
+            r += 1
+        
+        lst = lst[:l + 1]
+        
+        dummyN = ListNode()
+        curr = dummyN
+        
+        for num in lst:
+            curr.next = ListNode(num)
+            curr = curr.next
+        
+        return dummyN.next
+        
         
