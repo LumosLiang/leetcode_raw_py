d7de56039b9270edec35563249d0b9838e6126ee
lumos! <43442549+LumosLiang@users.noreply.github.com>
2022-03-05 14:25:50 +0800

    241 ms

--- solutions/344. Reverse String.py
+++ solutions/344. Reverse String.py
@@ -3,16 +3,31 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        if s == []: return None
+        return self.sol1(s)
+    
+    # O(N), O(1)
+    def sol1(self, s):
+        l, r = 0, len(s) - 1
         
-        pointer1, pointer2 = 0, len(s) - 1
+        while l < r:
+            s[l],s[r] = s[r], s[l]
+            l += 1
+            r -= 1
+    
+    # O(N), O(1)
+    def sol2(self, s):
         
-        while pointer1 <= pointer2:
-            s[pointer1], s[pointer2] = s[pointer2], s[pointer1]
-            pointer1 += 1
-            pointer2 -= 1
-        
-        return s
+        def helper(nums, left, right):
+            if left >= right: return
             
+            nums[left], nums[right] = nums[right], nums[left]
+            helper(nums, left + 1, right - 1)
+        
+        helper(s, 0, len(s) - 1)
+        
+        
+        
+        
+        
         
         
