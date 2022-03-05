92ae10e0ac1a0370e6d1d74caeeea589903ab743
lumos! <43442549+LumosLiang@users.noreply.github.com>
2022-03-05 13:52:33 +0800

    315 ms

--- solutions/704. Binary Search.py
+++ solutions/704. Binary Search.py
@@ -1,17 +1,30 @@
 class Solution:
-    def search(self, nums, target):
-        start = 0
-        end = len(nums) - 1
-​
-        def binary_search(arr, start, end, target):
-            if start > end:
-                return -1
-            mid = start + (end - start) // 2
-            if arr[mid] > target:
-                return binary_search(arr, start, mid - 1, target)
-            if arr[mid] < target:
-                return binary_search(arr, mid + 1, end, target)
-            return mid
+    def search(self, nums: List[int], target: int) -> int:
+        return self.sol1(nums, target)
+    
+    # typical O(logN), O(1)
+    def sol1(self, nums: List[int], target: int):
+        l, r = 0, len(nums) - 1
         
-        return binary_search(nums, start, end, target)
-​
+        while l <= r:
+            mid = (l + r) // 2
+            temp = nums[mid]
+            if temp == target: return mid
+            elif temp > target: r = mid - 1
+            else: l = mid + 1
+        
+        return -1
+        
+    # another way O(logN), O(1)
+    def sol2(self, nums: List[int], target: int):
+        
+        l, r = 0, len(nums) - 1
+        
+        while l < r:
+            mid = (l + r) // 2
+            temp = nums[mid]
+            
+            if temp >= target: r = mid
+            else: l = mid + 1
+        
+        return l if nums[l] == target else -1
