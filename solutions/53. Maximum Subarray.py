3e41dbf9804b6959e1ddb9b1f7761fbb1d08086e
lumos! <43442549+LumosLiang@users.noreply.github.com>
2022-03-05 18:10:47 +0800

    4107 ms

--- solutions/53. Maximum Subarray.py
+++ solutions/53. Maximum Subarray.py
@@ -1,39 +1,61 @@
 class Solution:
-    def maxSubArray(self, nums):
-     
+    def maxSubArray(self, nums: List[int]) -> int:
+        return self.DC(nums)
+#         This is a really good DP problem, like the high-vote solution mentioned: "The format of the sub problem can be helpful when we are trying to come up with the recursive relation."
+        
+#         subproblem or the state here is:
+        
+#           maximum val of dp[:i], 
+        
+#         so the dp function is:
+        
+#           dp[:i] = A[i] + dp[:i - 1] if dp[:i - 1] > 0 else 0
+        
+#         The base here is:
+        
+#           dp[0] = nums[0]
+    
+    # O(N), O(1)
+    def DP(self, nums):
+        if len(nums) == 1:return nums[0]
+    
+        res, pre = nums[0], nums[0]
+    
+        for i in range(1, len(nums)):
+            curr = nums[i] + (0 if pre < 0 else pre)
+            pre = curr
+            res = max(res, curr)
+            
+        return res
+    
+    # O(NlogN), O(logN)
+    def DC(self, nums):
+        
+        if len(nums) == 0: return float(-inf)
+        if len(nums) == 1: return nums[0]
+        
         low, high = 0, len(nums) - 1
         mid = low + (high - low) // 2
         
-        if nums == []:
-            return float("-inf")
-        if len(nums) == 1:
-            return nums[0]
-        
-        # leftarray
-        left = self.maxSubArray(nums[:mid])
-        # rightarray
-        right = self.maxSubArray(nums[mid + 1:])
-        # mid
-        middle = self.midSubArray(nums, mid, low, high)
+        left = self.DC(nums[:mid])
+        right = self.DC(nums[mid + 1:])
+        middle = self.helper(nums, mid, low, high)
         
         return max(middle, right, left)
         
-    def midSubArray(self, nums, m, l, h):
+    def helper(self, nums, m, l, h):
         res = nums[m]
-​
+        
         rsum = float("-inf")  
         Sum = 0
         for i in range(m + 1, h + 1):
             Sum += nums[i]
-            if Sum > rsum:
-                rsum = Sum
+            rsum = max(rsum, Sum)
             
         lsum = float("-inf")  
         Sum = 0
         for j in range(m - 1, l - 1, -1):
             Sum += nums[j]
-            if Sum > lsum:
-                lsum = Sum
-​
+            lsum = max(lsum, Sum)
+                
         return max(res, res + lsum, res + rsum, res + lsum + rsum)
-​
