ed153e1f51a96f1402c2123d8e49ae5b974d94cf
lumos! <43442549+LumosLiang@users.noreply.github.com>
2022-03-05 17:20:26 +0800

    87 ms

--- solutions/203. Remove Linked List Elements.py
+++ solutions/203. Remove Linked List Elements.py
@@ -7,6 +7,11 @@
 class Solution:
     def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
         
+        return self.sol2(head, val)
+        
+    # O(N), O(1)  
+    def sol1(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
+        
         pre = ListNode()
         pre.next = head
         curr = pre
@@ -16,4 +21,17 @@ class Solution:
             else: curr = curr.next
         
         return pre.next
+    
+    # O(N), O(N)(stack space) 
+    def sol2(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
         
+        if not head: return None
+        
+        if head.val == val:
+            head = self.sol2(head.next, val)
+        else:
+            head.next = self.sol2(head.next, val)
+        
+        return head
+        
+​
