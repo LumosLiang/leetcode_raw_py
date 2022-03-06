f1712c988f5c3e0b32d07a6b9a11092f5d5f5d43
lumos! <43442549+LumosLiang@users.noreply.github.com>
2022-03-06 19:06:27 +0800

    36 ms

--- solutions/46. Permutations.py
+++ solutions/46. Permutations.py
@@ -1,11 +1,12 @@
 class Solution:
-    
+    # O(N*N!), O(N!)
     def permute(self, nums):
         res = []
         
         def backtrack(choices, track):
             if not choices:
-                res.append(track[:])
+                res.append(track)
+                return
                 
             for i in range(len(choices)):
                 backtrack(choices[:i] + choices[i+1:], track + [choices[i]])
