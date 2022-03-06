b41a8dd4612db1c8b73b4f3c8d3c759aeadb0bb9
lumos! <43442549+LumosLiang@users.noreply.github.com>
2022-03-06 18:03:56 +0800

    140 ms

--- solutions/733. Flood Fill.py
+++ solutions/733. Flood Fill.py
@@ -1,24 +1,30 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, newColor: int) -> List[List[int]]:
+        # O(MN), O(MN)
         
         m, n = len(image), len(image[0])
         origin_color = image[sr][sc]
         
-        visited = [[-1 for i in range(n)] for j in range(m)]
+        # visited = [[-1 for i in range(n)] for j in range(m)]
         
         q = collections.deque([[sr, sc]])
         image[sr][sc] = newColor
-        visited[sr][sc] = 0
+        # visited[sr][sc] = 0
+        
+        if origin_color == newColor: return image
         
         while q:
             i, j = q.popleft()
             
             for x, y in [i + 1, j],[i - 1, j],[i, j + 1],[i, j - 1]:
-                if 0 <= x <= m - 1 and 0 <= y <= n - 1 and visited[x][y] != 0 and image[x][y] == origin_color:
+                if 0 <= x <= m - 1 and 0 <= y <= n - 1 and image[x][y] == origin_color:
                     image[x][y] = newColor
-                    visited[x][y] = 0
+                    # visited[x][y] = 0
                     q.append([x, y])
         
         return image
                 
+        # basic BFS
+        # remember to avoid duplicate: set the newcolor first before entering the q.
+        # remember to add visited if some dumplicate cannot be avoid by only comparing val. 
                 
