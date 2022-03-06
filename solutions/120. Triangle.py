bf204449f5c40eb1758f152a50c57e11b8f0ccc6
lumos! <43442549+LumosLiang@users.noreply.github.com>
2022-03-06 19:19:59 +0800

    68 ms

--- solutions/120. Triangle.py
+++ solutions/120. Triangle.py
@@ -1,6 +1,9 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
         
+        # pure DP problem
+        # dp[i][j] = min(dp[i - 1][j], dp[i][j]) + triangle[i][j]
+        
         init = triangle[0][0]
         
         l = len(triangle)
@@ -16,5 +19,6 @@ class Solution:
                 elif j == len(triangle[i]) - 1: temp.append(dp[j - 1] + triangle[i][j])
                 else: temp.append(min(dp[j - 1], dp[j]) + triangle[i][j])
             dp = temp
+            print(dp)
             
         return min(dp)
