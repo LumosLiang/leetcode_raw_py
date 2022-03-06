9e0933a743ccbabec8d3e905f5f8736b1505a655
lumos! <43442549+LumosLiang@users.noreply.github.com>
2022-03-06 19:14:24 +0800

    60 ms

--- solutions/198. House Robber.py
+++ solutions/198. House Robber.py
@@ -1,6 +1,10 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         
+        # O(N), O(1)
+        
+        # dp[i] = max(dp[i - 2] + nums[i], dp[i - 1])
+        
         if len(nums) == 1: return nums[0]
         
         curr, pre_1, pre_2 = 0, max(nums[1], nums[0]), nums[0]
@@ -11,5 +15,3 @@ class Solution:
             pre_1 = curr
             
         return curr if curr != 0 else pre_1
-            
-​
