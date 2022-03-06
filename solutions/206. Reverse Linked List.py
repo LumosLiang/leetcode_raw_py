82ba3cb774e14610f54f9d145a2805aca2c1fc17
lumos! <43442549+LumosLiang@users.noreply.github.com>
2022-03-06 18:37:22 +0800

    N/A

--- solutions/206. Reverse Linked List.py
+++ solutions/206. Reverse Linked List.py
@@ -5,11 +5,32 @@
 #         self.next = next
 ​
 class Solution:
-    def reverseList(self, head: ListNode) -> ListNode:
-        if not head or not head.next: 
-            return head
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         
-        last = self.reverseList(head.next)
+        return self.solution1(head)
+   
+    # recursive
+    # O(N), O(N)
+    def solution1(self, head):
+        
+        if not head or not head.next: return head
+        nh = self.solution1(head.next)
         head.next.next = head
         head.next = None
-        return last
+        
+        return nh
+        
+    # O(N), O(1)
+    def solution2(self, head):
+        
+        
+        pre, curr, nxt = None, head, None
+        
+        while curr:
+            nxt = curr.next
+            curr.next = pre
+            pre = curr
+            curr = nxt
+        
+        
+        return pre
