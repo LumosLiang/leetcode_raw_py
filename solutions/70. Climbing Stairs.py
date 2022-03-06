f6b2945302d40d072d8a2bca252dbef856c84042
lumos! <43442549+LumosLiang@users.noreply.github.com>
2022-03-06 19:10:18 +0800

    32 ms

--- solutions/70. Climbing Stairs.py
+++ solutions/70. Climbing Stairs.py
@@ -1,6 +1,10 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
         
+        # O(N), O(1)
+        
+        # dp(n) = dp(n - 1) + dp(n - 2)
+        
         if n <= 2: return n
         
         a, b, c = 1, 2, 0
@@ -11,4 +15,3 @@ class Solution:
             b = c
         
         return c
-            
