915678b9fe92423f9a9d855fd8696f4744f51030
lumos! <43442549+LumosLiang@users.noreply.github.com>
2022-03-09 21:30:08 +0800

    N/A

--- solutions/160. Intersection of Two Linked Lists.py
+++ solutions/160. Intersection of Two Linked Lists.py
@@ -1,41 +1,56 @@
 # Definition for singly-linked list.
 # class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
-
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+​
 class Solution:
-    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
-        if not headA or not headB:
-            return None
-        
-        lenA = lenB = 0
-        currA = headA
-        currB = headB
-        
-        while currA:
-            currA = currA.next
-            lenA += 1
-        
-        while currB:
-            currB = currB.next
-            lenB += 1
-        
-        currA = headA
-        currB = headB
-        if lenA > lenB:
-            while lenA - lenB > 0:
-                lenA -= 1
-                currA = currA.next
-        else:
-            while lenB - lenA > 0:
-                lenB -= 1
-                currB = currB.next
-        
-        while currA:
-            if currA is currB:
-                return currA
-            else:
-                currA = currA.next
-                currB = currB.next
-        
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        return self.sol0(headA, headB)
+    
+    # brute force + TLE
+    def sol0(self, headA: ListNode, headB: ListNode):
+        
+        c1= headA
+        
+        while c1:
+            c2 = headB
+            while c2:
+                if c1 == c2:
+                    return c1
+                c2 = c2.next
+            c1 = c1.next
+        return None
+    
+    # O(M+N), O(M+N)
+    # hash
+    def sol1(self, headA: ListNode, headB: ListNode):
+        
+        c1, c2 = headA, headB
+        s = set()
+        
+        while c1:
+            if c1 not in s:
+                s.add(c1)
+            c1 = c1.next
+        
+        while c2:
+            if c2 in s:
+                return c2
+            c2 = c2.next
+        return None
+    
+    # O(M+N), O(1)
+    # two pointer: fast, slow
+    def sol2(self, headA: ListNode, headB: ListNode):
+        
+        # 1 length
+        def callen(head):
+            if not head: return 0
+            curr, cnt = head, 0
+            while curr:
+                curr = curr.next
+                cnt += 1
+            return cnt
+        
+        # 2 make the same length start O(M + N)
