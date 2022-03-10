bd656e1c2e3951b00a96647f43c960831a75b4f2
lumos! <43442549+LumosLiang@users.noreply.github.com>
2022-03-10 22:33:31 +0800

    267 ms

--- solutions/64. Minimum Path Sum.py
+++ solutions/64. Minimum Path Sum.py
@@ -1,30 +1,83 @@
 class Solution:
-    
-    # backtrack + memo 
-    def minPathSumBS(self, grid: List[List[int]]) -> int:
-        pass
-        
-        
-    # DP bottom-up
     def minPathSum(self, grid: List[List[int]]) -> int:
+​
+        return self.BFS(grid)
+        
+    # all about backtrack/DFS/recursion
+    def sol0(self, grid):
+        # every step has two choices
+        m, n = len(grid), len(grid[0])
+        
+        def backtrack(i, j):
+            
+            if i == m - 1 and j == n - 1:
+                return grid[i][j]
+            
+            if j == n - 1 and 0 <= i < m - 1:
+                return grid[i][j] + backtrack(i + 1, j)
+            
+            if i == m - 1 and 0 <= j < n - 1:
+                return grid[i][j] + backtrack(i, j + 1)
+            
+            if 0 <= j < n - 1 and 0 <= i < m - 1:
+                return grid[i][j] + min(backtrack(i + 1, j), backtrack(i, j + 1))
+        
+        return backtrack(0, 0)
+        
+    # recursion with memo
+        
+    def memo1(self, grid):
+        # every step has two choices
+        m, n = len(grid), len(grid[0])
+        
+        @lru_cache(None)
+        def backtrack(i, j):
+            
+            if i == m - 1 and j == n - 1:
+                return grid[i][j]
+            
+            if j == n - 1 and 0 <= i < m - 1:
+                return grid[i][j] + backtrack(i + 1, j)
+            
+            if i == m - 1 and 0 <= j < n - 1:
+                return grid[i][j] + backtrack(i, j + 1)
+            
+            if 0 <= j < n - 1 and 0 <= i < m - 1:
+                return grid[i][j] + min(backtrack(i + 1, j), backtrack(i, j + 1))
+        
+        return backtrack(0, 0)
+    
+    def memo2(self, grid):
+        # every step has two choices
+        m, n = len(grid), len(grid[0])
+        
+        memo = {}
+        def backtrack(i, j):
+            
+            if (i,j) in memo: return memo[(i,j)]
+            
+            memo[(i,j)] = -1
+            if i == m - 1 and j == n - 1:
+                memo[(i,j)] = grid[i][j]
+            
+            if j == n - 1 and 0 <= i < m - 1:
+                memo[(i,j)] = grid[i][j] + backtrack(i + 1, j)
+            
+            if i == m - 1 and 0 <= j < n - 1:
+                memo[(i,j)] = grid[i][j] + backtrack(i, j + 1)
+            
+            if 0 <= j < n - 1 and 0 <= i < m - 1:
+                memo[(i,j)] = grid[i][j] + min(backtrack(i + 1, j), backtrack(i, j + 1))
+            
+            return memo[(i,j)]
+        
+        return backtrack(0, 0)
+    
+    # DP backward, forward. 
+    
+    def forward(self, grid):
         
         m, n = len(grid), len(grid[0])
         
-        dp = [[0 for i in range(n)] for j in range(m)]
-        
-        dp[m - 1][n - 1] = grid[m - 1][n - 1]
-        
-        for i in range(n - 1, -1, -1):
-            if i < n - 1:
-                dp[m - 1][i] = grid[m - 1][i] + dp[m - 1][i + 1]
-        
-        for i in range(m - 1, -1, -1):
-            if i < m - 1:
-                dp[i][n - 1] = grid[i][n - 1] + dp[i + 1][n - 1]
-        
-        
-        for i in range(m - 2, -1, -1):
-            for j in range(n - 2, -1, -1):
-                dp[i][j] = grid[i][j] + min(dp[i + 1][j], dp[i][j + 1])
-            
-        return dp[0][0]
+        for i in range(m):
+            for j in range(n):
