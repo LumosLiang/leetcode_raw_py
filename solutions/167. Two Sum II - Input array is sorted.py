fb895292c96c6a13d8ccb5076b605192d7a7ae91
LumosLiang <lsllywjb@gmail.com>
2022-03-17 17:59:09 +0800

    test

--- solutions/167. Two Sum II - Input array is sorted.py
+++ solutions/167. Two Sum II - Input array is sorted.py
@@ -1,13 +1,28 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
         
-        if len(numbers) <= 1: return []
+        return self.solution2(numbers, target)
+    
+    # T O(N), S: O(1)
+    def solution1(self, numbers: List[int], target: int):
         
-        hash = {}
+        l, r = 0, len(numbers) - 1
+        
+        while l < r:
+            s = numbers[l] + numbers[r]
+            if s == target: return [l + 1, r + 1]
+            elif s > target: r -= 1
+            else: l += 1
+    
+    # T: O(N), S: O(N)
+    def solution2(self, numbers: List[int], target: int):
+        
+        H = {}
         
         for i in range(len(numbers)):
-            if numbers[i] in hash:
-                return [hash[numbers[i]] + 1, i + 1]
+            if numbers[i] in H:
+                return [H[numbers[i]] + 1, i + 1]
             else:
-                hash[target - numbers[i]] = i
+                H[target - numbers[i]] = i
                 
+        
