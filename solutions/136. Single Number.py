dd752142c540f90196196d73ddc9df2cae7624a6
lumos! <43442549+LumosLiang@users.noreply.github.com>
2022-03-28 22:02:09 +0800

    171 ms

--- solutions/136. Single Number.py
+++ solutions/136. Single Number.py
@@ -1,10 +1,35 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         
-        # xor
+        # hash, set, addition space
+        # O(N), O(N)
         
-        res = 0
-        for num in nums:
-            res ^= num
+        
+        # Sort()
+        # O(NlogN), O(1)
+        
+        
+        # brute force
+        # O(N * N), O(1)
+        
+        
+        # bit manipulation: XOR, move
+        # O(N), O(1)
+        
+        res = nums[0]
+        
+        for i in range(1, len(nums)):
+            res ^= nums[i]
+        
         return res
-            
+    
+    
+        res = 0
+        
+        for i in range(1, len(nums)):
+            res ^= nums[i]
+        
+        return res
+        
+        
+        
