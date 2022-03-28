3cebd2acb8e38c44bbfb95b3dbcefc8d931abae7
lumos! <43442549+LumosLiang@users.noreply.github.com>
2022-03-28 22:46:31 +0800

    69 ms

--- solutions/75. Sort Colors.py
+++ solutions/75. Sort Colors.py
@@ -3,6 +3,29 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        
+        # pointers, real deal is p2
+        
+        
+        p1, p2, p3 = 0, len(nums) - 1, len(nums) - 1
+        
+        while p2 >= p1:
+            
+            if nums[p2] == 0:
+                nums[p2], nums[p1] = nums[p1], nums[p2]
+                p1 += 1
+            elif nums[p2] == 2:
+                nums[p2], nums[p3] = nums[p3], nums[p2]
+                p2 -=1
+                p3 -= 1
+            else:
+                p2 -= 1
+                
+​
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
         start, mid, end = 0, 0, len(nums) - 1
         
         while mid <= end:
@@ -17,3 +40,6 @@ class Solution:
                 end -= 1
                 
         return nums
+                
+        
+        
