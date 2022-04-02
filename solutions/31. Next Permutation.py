229e10077bdf906bdb903efe12982bc41f321fe1
lumos! <43442549+LumosLiang@users.noreply.github.com>
2022-04-02 20:49:03 +0800

    73 ms

--- solutions/31. Next Permutation.py
+++ solutions/31. Next Permutation.py
@@ -3,29 +3,44 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        l = len(nums) 
-        i, j = l - 2, l - 1 
+        # Two pointers + Greedy thinkings? This is really done by myself. Good for me!
         
-        while i >= 0 and nums[i] >= nums[j]:
+        # O(N), O(1)
+        
+        def rev(nums, l, r):
+            while l <= r:
+                nums[l], nums[r] = nums[r], nums[l]
+                l += 1
+                r -= 1
+        
+        length = len(nums)
+        i, j = length - 2, length - 1
+        
+        # First search for the first adjacent ones that nums[i] < nums[j]
+        while i >= 0 and j >= 1 and nums[i] >= nums[j]:
             i -= 1
             j -= 1
+            
+        # Then locate i, use j to search for the right side to find the smallest larger one than nums[i]
+        # but before that, let's just do a check to see if it is decreasing list, if it is, let's just directly reverse it all
         
-        if i != -1:
-            while j <= l - 1:
-                if nums[j] > nums[i]:
-                    j += 1
-                else:
-                    nums[j - 1], nums[i] = nums[i], nums[j - 1]
-                    break
-            if j == l:
+        if i == -1: 
+            rev(nums, 0, length - 1)
+            return
+            
+        while j <= length:
+            if j == length: 
                 nums[j - 1], nums[i] = nums[i], nums[j - 1]
+                break
+            if nums[j] <= nums[i]:
+                nums[j - 1], nums[i] = nums[i], nums[j - 1]
+                break
+            j += 1
         
-        start, end = i + 1, l - 1
-        while start < end:
-            nums[start], nums[end] = nums[end], nums[start]
-            start += 1
-            end -= 1
-​
-       
+        # reverse the rest after i
+        rev(nums, i + 1, length - 1)
+        
+        
+            
         
         
