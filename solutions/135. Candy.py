4ec1593cb0fbe5505ceefa0753d4d4e41730689f
lumos! <43442549+LumosLiang@users.noreply.github.com>
2022-04-18 15:57:50 +0800

    169 ms

--- solutions/135. Candy.py
+++ solutions/135. Candy.py
@@ -3,28 +3,51 @@ class Solution:
 ​
         # O(N), O(N)
         
-        res = [0] * len(ratings)
-        res[0] = 1
+        res = [1] * len(ratings)
         
         # forward
         
         for i in range(len(ratings) - 1):
-            curr, nxt = ratings[i], ratings[i + 1]
-            if curr < nxt:
+            if ratings[i] < ratings[i + 1]:
                 res[i + 1] = res[i] + 1
-            else:
-                res[i + 1] = 1
-        
-        print(res)
-        
+           
         # backward, fix
         
         for i in range(len(ratings) - 1, 0, -1):
-            curr, pre = ratings[i], ratings[i - 1]
-​
-            if curr < pre:
-                if res[i] >= res[i - 1]:
-                    res[i - 1] = res[i] + 1
+            if ratings[i] < ratings[i - 1] and res[i] >= res[i - 1]:
+                res[i - 1] = res[i] + 1
          
-        print(res)
         return sum(res)
+    
+# class solution:
+#     def candy(self, ratings: list[int]) -> int:
+​
+#         # o(n), o(n)
+        
+#         res = [0] * len(ratings)
+#         res[0] = 1
+        
+#         # forward
+        
+#         for i in range(len(ratings) - 1):
+#             curr, nxt = ratings[i], ratings[i + 1]
+#             if curr < nxt:
+#                 res[i + 1] = res[i] + 1
+#             elif curr == nxt:
+#                 res[i + 1] = 1
+#             else:
+#                 if curr > 1:
+#                     res[i + 1] = res[i] - 1
+#                 else:
+#                     res[i + 1] = 1
+        
+#         print(res)
+        
+#         # backward, fix
+        
+#         for i in range(len(ratings) - 1, 0, -1):
+#             curr, pre = ratings[i], ratings[i - 1]
+​
+#             if curr < pre:
+#                 if res[i] == res[i - 1]:
+#                     res[i - 1] = res[i] + 1
