baa56ff1897f92a8f74dedd016c17e8a5487ecd5
lumos! <43442549+LumosLiang@users.noreply.github.com>
2022-04-21 09:27:32 +0800

    70 ms

--- solutions/25. Reverse Nodes in k-Group.py
+++ solutions/25. Reverse Nodes in k-Group.py
@@ -7,28 +7,66 @@
 class Solution:
     def reverseKGroup(self, head, k):
         
-        if k is None: return None
-        
-        # base
+        return self.recursive(head,k)
+​
+    # O(N), O(N//k)
+    def recursive(self, head, k):
+        if not head: return None
+    
+        cnt = 1
         curr = head
-        for _ in range(k):
-            if curr is None: return head
+        
+        while curr and cnt < k:
+            cnt += 1
             curr = curr.next
         
-        self.successor = ListNode()
-        new_head = self.reverseFirstK(head, k)
-        nxt = self.reverseKGroup(self.successor, k)
-        head.next = nxt
-        return new_head
+        if not curr: return head
         
-    def reverseFirstK(self, head, k):
-        if k == 1:
-            self.successor = head.next
-            return head
-            
-        first = self.reverseFirstK(head.next, k - 1)
-        head.next.next = head
-        head.next = self.successor
-        return first
+        nxt = curr.next
+        curr.next = None
+        nh = self.reverse(head)
+        
+        head.next = self.reverseKGroup(nxt, k)
+        
+        return nh
+    
+    # O(k), O(1)
+    def reverse(self, head):
+        if not head: return None
+        
+        pre, curr, nxt = None, head, None
+        
+        while curr:
+            nxt = curr.next
+            curr.next = pre
+            pre = curr
+            curr = nxt
+        
+        return pre
+    
+    # O(N), O(1)
 ​
+    # dummy
+​
+    # 每一次我都取n, n+k的node， 
+    # start, trail
+    # 但是同时，要记录，该段翻转的前一个点和后一个点，pre, tnxt.
+    # in-place reverse: 注意的点就是要，确定好新的 head, 和tail
+​
+    # start.pre.next = trail.
+    # start.next = trail.next
+    # 再次记录头
+​
+    # 取下一组K，
+​
+    # 如果取不到，就直接return dummy.next
+​
+    # 如果题目要求是 无论不足也翻转的话，那这种事情，应该只出现一次，就是在最后一组。
+    # 如果遇到取不到k，
+    # 就也翻转一次。
+​
+    def iterative(self, head, k):
         
+        pass
+        
+            
