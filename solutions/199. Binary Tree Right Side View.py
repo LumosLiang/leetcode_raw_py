48fe010d2a202a5e3b5c41e01496b9f45f555b4f
lumos! <43442549+LumosLiang@users.noreply.github.com>
2022-04-21 20:53:49 +0800

    64 ms

--- solutions/199. Binary Tree Right Side View.py
+++ solutions/199. Binary Tree Right Side View.py
@@ -5,16 +5,55 @@
 #         self.left = left
 #         self.right = right
 ​
-​
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
         
+        # BFS 不要太简单
         
+        return self.DFS2(root)
+        
+        
+    def DFS1(self, root):
+        # BFS 不要太简单
+        
+        # DFS, essentially left, right, middle, post-order traversal
         if not root: return []
         
-    
         left = self.rightSideView(root.left)
         right = self.rightSideView(root.right)
         
-        if len(right) >= len(left): return [root.val] + right
-        else: return [root.val] + right + left[len(right):]
+        ll, rl = len(left), len(right)
+        
+        if rl >= ll: return [root.val] + right
+        else: return [root.val] + right + left[rl:]
+        
+    
+    def DFS2(self, root):
+        
+        # root, left, right, pre-order traversal
+        
+        self.res = []
+        
+        def helper(root, level):
+            
+            # base
+            if not root: return
+            
+            if level > len(self.res):
+                self.res.append(root.val)
+            
+            helper(root.right, level + 1)
+            helper(root.left, level + 1)
+            
+        helper(root, 1)
+            
+        return self.res
+            
+            
+            
+            
+        
+        
+        
+        
+​
