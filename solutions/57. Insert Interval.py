c5854130a6a0480b67765f7be6349f7a85ba4a48
lumos! <43442549+LumosLiang@users.noreply.github.com>
2022-05-04 21:37:07 +0800

    152 ms

--- solutions/57. Insert Interval.py
+++ solutions/57. Insert Interval.py
@@ -1,40 +1,16 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-    
-    #    ---   ---- ----- ----- - --- --
-    #        ---------------
-    # --
-    #        -
-    #                --
-​
-    # if nI[end] < interval[0][0] or if nI[start] > interval[-1][1]
-​
-    # loop each interval, [left, right], 
-    
-    
-        if not intervals: return [newInterval]
+        
+        
         res = []
         
-        start, end = newInterval[0], newInterval[1]
-        
-        flag_set = False
         for i in range(len(intervals)):
-            if flag_set:
-                res.append(intervals[i])  
+            if newInterval[1] < intervals[i][0]:
+                res.append(newInterval)
+                return res + intervals[i:]
+            elif newInterval[0] > intervals[i][1]:
+                res.append(intervals[i])
             else:
-                if start > intervals[i][1]: 
-                    res.append(intervals[i])
-                else:
-                    if end < intervals[i][0]:
-                        res.append([start, end])
-                        res.append(intervals[i])
-                        flag_set = True
-                    else:
-                        start = min(start, intervals[i][0]) 
-                        end = max(end, intervals[i][1])
-        
-        if not flag_set: return res + [[start, end]]
-        return res
-        
-    
-            
+                newInterval = [min(newInterval[0], intervals[i][0]), max(newInterval[1], intervals[i][1])]
+                
+        return res + [newInterval]
