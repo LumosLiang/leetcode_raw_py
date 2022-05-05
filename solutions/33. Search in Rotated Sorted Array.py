39ce516b9c5066eadb9073eabe96f0498e47b2ec
lumos! <43442549+LumosLiang@users.noreply.github.com>
2022-05-05 18:09:01 +0800

    41 ms

--- solutions/33. Search in Rotated Sorted Array.py
+++ solutions/33. Search in Rotated Sorted Array.py
@@ -3,21 +3,33 @@ class Solution:
         
         l, r = 0, len(nums) - 1
         
+        # [6,7,0,1,2,3,4,5]
+        # [3,4,5,6,7,0,1,2]
+        # decide which part mid locate
+        # decide which part target locate
+        
+        # 先归到一个确定的区间内。
+        
         while l <= r:
-            mid = l + (r - l + 1) // 2
+            mid = (l + r) // 2
+            
+            # for 更快
             if nums[mid] == target: return mid
             
-            if nums[r] >= nums[mid]:
-                if nums[mid] < target <= nums[r]:
-                    l = mid + 1
-                else:
-                    r = mid - 1
-​
+            # 归区间
+            if nums[mid] > nums[r] and target <= nums[r]:
+                l = mid + 1
+            elif nums[mid] < nums[r] and target > nums[r]:
+                r = mid - 1
             else:
-                if nums[l] <= target < nums[mid]:
-                    r = mid - 1
-                else:
+                # 做二分
+                if nums[mid] == target:
+                    return mid
+                elif nums[mid] < target:
                     l = mid + 1
-                
+                else:
+                    r = mid - 1
+            
         return -1
-                    
+    
+    
