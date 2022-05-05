479e222aa93207b6445d38eff1a0d42fe6c20535
lumos! <43442549+LumosLiang@users.noreply.github.com>
2022-05-05 21:14:24 +0800

    48 ms

--- solutions/162. Find Peak Element.py
+++ solutions/162. Find Peak Element.py
@@ -3,6 +3,9 @@ class Solution:
         
         l, r = 0, len(nums) - 1
         
+        # 还是划分到自己的区间内，
+        # 斜率比较，还是要观察这种insight
+        
         while l < r:
             mid = (l + r) // 2
             
