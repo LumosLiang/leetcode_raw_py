aff3dbb84bcfb4fed6167783c702be5f32634723
lumos! <43442549+LumosLiang@users.noreply.github.com>
2022-05-05 21:13:24 +0800

    48 ms

--- solutions/162. Find Peak Element.py
+++ solutions/162. Find Peak Element.py
@@ -1,19 +1,15 @@
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
- 
-        lo, hi = 0, len(nums) - 1
         
-        while lo < hi:
-            mid = lo + (hi - lo) // 2
-​
-            if hi - lo == 1:
-                if nums[mid] < nums[mid + 1]:return mid + 1
-                else: return mid
+        l, r = 0, len(nums) - 1
+        
+        while l < r:
+            mid = (l + r) // 2
+            
+            if nums[mid] < nums[mid + 1]:
+                l = mid + 1
             else:
-                if nums[mid - 1] < nums[mid] and nums[mid] > nums[mid + 1]: return mid
-                elif nums[mid - 1] < nums[mid]: lo = mid
-                elif nums[mid] > nums[mid + 1]: hi = mid
-                else: lo = mid 
+                r = mid
         
-        return hi
-​
+        return l
+            
