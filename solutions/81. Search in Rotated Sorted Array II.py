72fbe6851c5c617ea1be62ba26eb259fe7282903
lumos! <43442549+LumosLiang@users.noreply.github.com>
2022-05-05 18:09:04 +0800

    103 ms

--- solutions/81. Search in Rotated Sorted Array II.py
+++ solutions/81. Search in Rotated Sorted Array II.py
@@ -1,23 +1,35 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> bool:
-        # Initilize two pointers
-        begin = 0
-        end = len(nums) - 1 
-        while begin <= end:
-            mid = (begin + end)//2
-            if nums[mid] == target:
-                return True
-            if nums[mid] == nums[end]: # Fail to estimate which side is sorted
-                end -= 1  # In worst case: O(n)
-            elif nums[mid] > nums[end]: # Left side of mid is sorted
-                if  nums[begin] <= target and target < nums[mid]: # Target in the left side
-                    end = mid - 1
-                else: # in right side
-                    begin = mid + 1
-            else: # Right side is sorted
-                if  nums[mid] < target and target <= nums[end]: # Target in the right side
-                    begin = mid + 1
-                else: # in left side
-                    end = mid - 1
-        return False
-​
+        
+        
+        # 先归到一个确定的区间内。
+        
+        # [2,2,5,0,2,2,2,2,2], 0
+        
+        # 方案1：先去掉，但是更改原队列感觉不太好
+        # while len(nums) > 1 and nums[-1] == nums[0]: nums.pop()
+        
+        l, r = 0, len(nums) - 1
+        
+        while l < r:
+            # 方案2：和方案1差不多，但是移动left去除元素（移动right 也行）
+            
+            while l + 1 < r and nums[l] == nums[l + 1]:
+                l += 1
+            
+            mid = (l + r) // 2
+            
+            # 归区间
+            if nums[mid] > nums[r] and target <= nums[r]:
+                l = mid + 1
+            elif nums[mid] <= nums[r] and target > nums[r]:
+                r = mid - 1
+            # 做二分
+            else:
+                if nums[mid] >= target:
+                    r = mid
+                elif nums[mid] < target:
+                    l = mid + 1
+        
+        return nums[l] == target
+    
