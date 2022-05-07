ded26d36e297a0ab984322233d62837e4517385e
lumos! <43442549+LumosLiang@users.noreply.github.com>
2022-05-07 11:45:48 +0800

    264 ms

--- solutions/240. Search a 2D Matrix II.py
+++ solutions/240. Search a 2D Matrix II.py
@@ -1,19 +1,24 @@
 class Solution:
-    def searchMatrix(self, matrix, target):
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
         
-        if not matrix: return False
+        # binary search Tree in matrix
         
-        l = len(matrix[0]) - 1
-        w = 0
+        # O(m + n), O(1)
         
-        while l >= 0 and w <= len(matrix) - 1:
-            if matrix[w][l] == target:
-                return True
-            elif matrix[w][l] > target:
-                l -= 1
-            elif matrix[w][l] < target:
-                w += 1
+        m, n = len(matrix), len(matrix[0])
+        
+        x, y = 0, n - 1
+        
+        while 0 <= x < m and 0 <= y < n:
             
+            if matrix[x][y] == target: return True
+            if matrix[x][y] < target:
+                x += 1
+            else:
+                y -= 1
+                
         
         return False
-​
+            
+        
+        
