dba527e9f0b32f9babaf5d64c0f1ecc92aa0530b
lumos! <43442549+LumosLiang@users.noreply.github.com>
2022-05-07 15:13:00 +0800

    294 ms

--- solutions/378. Kth Smallest Element in a Sorted Matrix.py
+++ solutions/378. Kth Smallest Element in a Sorted Matrix.py
@@ -1,8 +1,8 @@
 class Solution:
     def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
-        return self.sol1(matrix, k)
+        return self.sol2(matrix, k)
         
-    # binary seach
+    # binary seach O(k * log(Diff))
     def sol1(self, matrix: List[List[int]], k: int):
         
         n = len(matrix)
@@ -36,4 +36,26 @@ class Solution:
             else: left = mid + 1
 ​
         return left
+    
+    # heap
+    # typical heap is loop all and pick the top 
+    # question now is how we can make use of the sorted matrix
+    
+    # O(klogK), O(k)
+    def sol2(self, matrix: List[List[int]], k: int):
+        n = len(matrix)
+        heap = [(matrix[0][0], 0, 0)]
+        visited = set()
+        visited.add((0, 0))
         
+        for i in range(k - 1):
+            val, x, y = heapq.heappop(heap)
+            if x + 1 < n and (x + 1, y) not in visited:
+                heapq.heappush(heap, (matrix[x + 1][y], x + 1, y))
+                visited.add((x + 1, y))
+            if y + 1 < n and (x, y + 1) not in visited:
+                heapq.heappush(heap, (matrix[x][y + 1], x, y + 1))
+                visited.add((x, y + 1))
+        
+        return heap[0][0]
+            
