9c2c2f5ee172950f8e45e94639f1ad8e06d4d703
lumos! <43442549+LumosLiang@users.noreply.github.com>
2022-05-09 15:21:22 +0800

    4748 ms

--- solutions/79. Word Search.py
+++ solutions/79. Word Search.py
@@ -1,4 +1,44 @@
 class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        
+        m, n = len(board), len(board[0])
+        
+        # return
+        def backtrack(x, y, word, cnt):
+            nonlocal board
+            
+            if cnt == len(word):
+                return True
+            
+            if board[x][y] != word[cnt]: 
+                return False
+            
+            
+            val = word[cnt]
+            board[x][y] = 0
+            
+            for i, j in [x + 1, y],[x - 1, y],[x, y + 1],[x, y - 1]:
+                if 0 <= i < m and 0 <= j < n and board[i][j]:
+                    temp = backtrack(i, j, word, cnt + 1)
+                    if temp: return True
+​
+            board[x][y] = val
+            
+            if cnt + 1 == len(word): return True
+            return False
+        
+        
+        for i in range(m):
+            for j in range(n):
+                
+                temp = backtrack(i, j, word, 0)
+                
+                if temp: return True
+        
+        return False
+        
+​
+class Solution:
     def exist(self, board, word):
         if not board:
             return False
@@ -7,7 +47,7 @@ class Solution:
                 if self.dfs(board, i, j, word):
                     return True
         return False
-​
+•
     # check whether can find word, start at (i,j) position    
     def dfs(self, board, i, j, word):
         if len(word) == 0: # all the characters are checked
@@ -21,9 +61,6 @@ class Solution:
         or self.dfs(board, i, j+1, word[1:]) or self.dfs(board, i, j-1, word[1:])
         board[i][j] = tmp
         return res
-        
-        
-        
         
         
         
