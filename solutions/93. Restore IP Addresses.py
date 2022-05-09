3a6e0f3d0aad137d0a4b977f25f4cb705a34013b
lumos! <43442549+LumosLiang@users.noreply.github.com>
2022-05-09 21:34:32 +0800

    96 ms

--- solutions/93. Restore IP Addresses.py
+++ solutions/93. Restore IP Addresses.py
@@ -1,30 +1,39 @@
 class Solution:
     def restoreIpAddresses(self, s: str) -> List[str]:
         
+        # backtrack
+        
+#         "101023"
+#         /   |   \
+#        1   10     101 
+#   / | \  / | \    / | \
+#  0  X X  1 10 102 0 X  X
+​
+#  1. 每一步都有选择，最多可以选3个digit，最少选一个
+#  2. 如果这一步是0，这一步只能选0，没有其他选择
+#  3. 必须要有四块，才能符合要求
+​
         res = []
         
-        def backtrack(choice, path, cnt):
-            # stop condition
-            # exactly four and no choice left
-            if cnt == 4 and len(choice) == 0:
-                res.append(path[:-1])
+        # backtrack 是当前 在idx处，你可以给我多少个答案
+        def backtrack(idx, path, cnt):
+            nonlocal s
             
-            # loop all choices
-            for i in range(len(choice)):
-                if self.isValidIP(choice[:i + 1]):
-                    backtrack(choice[i + 1:], path + choice[:i + 1] + ".", cnt + 1)
+            if idx == len(s):
+                if cnt == 4: 
+                    res.append(path.rstrip('.'))
+                return
+            
+            if s[idx] == "0":
+                backtrack(idx + 1, path + s[idx] + ".", cnt + 1)
+                return
+            
+            for i in range(1, 4):
+                if idx + i < len(s) + 1:
+                    temp = s[idx: idx + i]
+                    if int(temp) < 256:
+                        backtrack(idx + i, path + temp + ".", cnt + 1)
                 
-        backtrack(s, "", 0)
-        
+               
+        backtrack(0, "", 0)
         return res
-    
-    def isValidIP(self, IP):
-        # each integer is between 0 ~ 255 and cannot have leading zeros.
-        
-        if len(IP) > 4: return False
-        
-        if len(IP) > 1 and IP[0] == "0": return False
-        
-        if int(IP) > 255 or int(IP) < 0: return False
-        
-        return True
