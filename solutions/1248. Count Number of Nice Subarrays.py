4ce5c54001a72a454631cea6ef897161269843aa
lumos! <43442549+LumosLiang@users.noreply.github.com>
2022-05-13 16:01:44 +0800

    991 ms

--- solutions/1248. Count Number of Nice Subarrays.py
+++ solutions/1248. Count Number of Nice Subarrays.py
@@ -1,21 +1,29 @@
 class Solution:
     def numberOfSubarrays(self, nums: List[int], k: int) -> int:
         
-        def atMost(k):
-            start, res = 0, 0
-            
-            for end, val in enumerate(nums):
-                if val % 2: k -= 1
-                    
-                while k < 0:
-                    if nums[start] % 2:k += 1
-                    start += 1
-                res += end - start + 1
-            
+        # 依然是同样的思路
+        # 发现无法满足 sliding window的条件 
+        # 加constraint
+        
+        # <= 2 - <= 1
+        # nums = [2,1,2,1,2]
+        # 1 + 2 + 3 + 4 + 5 - (1 + 2 + 3 + 2 + 3) = 15 - 11 = 4
+        
+        # Return the number of sub-arrays where there are less or equal tan k odd numbers on it
+        def helper(nums, k):
+            l = 0
+            res = 0
+            window_cnt = 0
+            for r, val in enumerate(nums):
+                if val % 2:
+                    window_cnt += 1
+                
+                while window_cnt > k:
+                    if nums[l] % 2:
+                        window_cnt -= 1
+                    l += 1
+                
+                res += r - l + 1
             return res
-        
-        return atMost(k) - atMost(k - 1)
-        
-        
-        
-        
+    
+        return helper(nums, k) - helper(nums, k - 1)
