37628ecb4447547f08b952838e52c19113178add
lumos! <43442549+LumosLiang@users.noreply.github.com>
2022-05-14 15:33:54 +0800

    49 ms

--- solutions/24. Swap Nodes in Pairs.py
+++ solutions/24. Swap Nodes in Pairs.py
@@ -3,24 +3,52 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+​
 class Solution:
-    def swapPairs(self, head: ListNode) -> ListNode:
+    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        return self.recursive(head)
         
-        if not head: return None
+    def iterative(self, head):
         
-        if not head.next: return head
+        # dummy -> 1 -> 2 -> 3
+        # dummy -> 2 -> 1 -> 3
         
-        pre, curr, nxt = None, head, None
-        head = head.next 
+        dummy = ListNode()
+        dummy.next = head
+    
+        pre, curr, nxt = dummy, head, None
         
         while curr and curr.next:
+            # curr node's next node
             nxt = curr.next
-            if pre:
-                pre.next = nxt
-            curr.next = nxt.next
+            # next node's next node - nxt_nxt
+            nxt_nxt = nxt.next
+            
+            # set the prev node's next to next node
+            pre.next = nxt
+            # set the prev node's next to curr node - swap
             nxt.next = curr
+            # set the curr node's next to new next node
+            curr.next = nxt_nxt
+            
+            # move to next state
             pre = curr
             curr = curr.next
-            
-        return head
+        
+        return dummy.next
+        
+    
+    # post-order
+    def recursive(self, head):
+       
+        if not head or not head.next: return head
+        
+        nxt_pair_h = self.recursive(head.next.next)
+        
+        nh = head.next
+        nh.next = head
+        head.next = nxt_pair_h
+        
+        return nh
+        
         
