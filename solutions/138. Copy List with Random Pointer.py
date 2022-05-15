256398c629ea0a2a97d4d85f227bb28e0db28316
lumos! <43442549+LumosLiang@users.noreply.github.com>
2022-05-15 18:18:52 +0800

    69 ms

--- solutions/138. Copy List with Random Pointer.py
+++ solutions/138. Copy List with Random Pointer.py
@@ -8,34 +8,70 @@ class Node:
 """
 ​
 class Solution:
-    def copyRandomList(self, head: 'Node') -> 'Node':
+    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
         
-        # hash table
+        return self.sol2(head)
         
-        # loop each node, generate its clone. and put the pair in the hash table
-        # loop each node, use its clone's next to next's clone's next
-        # loop each node, use its random
-        
+    # O(N), O(N)
+    def sol1(self, head):
         if not head: return None
+   
+        # 1. if want to deep_copy, then just loop, at every step, just continue create new ListNode
+        # 2. but questions is how to link with the correct one on random and next field, instead of creating incorrect new              ones.
+        # 3. use hash to store this match relationship
         
-        hash = {}
+        # hash = [key:(next, random, new)]
+​
+        h = {}
         curr = head
         
         while curr:
-            hash[curr] = Node(curr.val)
+            h[curr] = [curr.next, curr.random, Node(curr.val)]
             curr = curr.next
+        
+        curr = head
+        while curr:
+            if curr.next: h[curr][2].next = h[curr.next][2]
+            else: h[curr][2].next = None
+            if curr.random: h[curr][2].random = h[curr.random][2]
+            else: h[curr][2].random = None
+            curr = curr.next
+        
+        return h[head][2]
             
-        curr1 = head
+    # O(N), O(1)    
+    def sol2(self, head):
         
-        while curr1:
-            if curr1.next:
-                hash[curr1].next = hash[curr1.next]
-            if curr1.random:
-                hash[curr1].random = hash[curr1.random]
-            curr1 = curr1.next
+        if not head: return None
         
-        return hash[head] 
+        curr, nxt = head, None
+        
+        # directly "insert" the copied node 
+        while curr:
+            nxt = curr.next
+            curr.next = Node(curr.val)
+            curr.next.next = nxt
+            curr = nxt
         
         
+        # link random
+        curr = head
+        while curr:
+            if curr.random:
+                curr.next.random = curr.random.next
+            curr = curr.next.next
         
+        # split each other, refer: odd/even linked list
+        # 7 -> 7' -> 13 -> 13' -> 11 -> 11' -> 10 -> 10' -> 1 -> 1'
+        
+        new_head = head.next
+        curr, curr_new = head, head.next
+        
+        while curr_new.next:
+            curr.next = curr.next.next
+            curr_new.next = curr_new.next.next
+            curr = curr.next
+            curr_new = curr_new.next
+        
+        return new_head
         
