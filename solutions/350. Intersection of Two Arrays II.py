bc210f520a79602f17f2ccca86b23c74a9fd1710
lumos! <43442549+LumosLiang@users.noreply.github.com>
2022-05-15 16:14:07 +0800

    128 ms

--- solutions/350. Intersection of Two Arrays II.py
+++ solutions/350. Intersection of Two Arrays II.py
@@ -1,6 +1,25 @@
 class Solution:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
-​
+        
+        # phase 1, think of it
+            # O(NlogN + MlogM + O(min(M, N))), O(1): two pointers
+            # O(N + M), O(N + M): hash
+        
+        # phase 2, What if the given array is already sorted? How would you optimize your algorithm?
+            # no need to sort, directly use two pointers
+            # O(min(M, N)), O(1)
+            
+        # phase 3, What if nums1's size is small compared to nums2's size? Which algorithm is better?
+            
+            # [1,2,2,4]
+            # [1,2,3,5,7,8]
+            # binary search
+            
+        # phase 4, What if elements of nums2 are stored on disk, and the memory is limited such that you cannot load all elements into the memory at once?
+            
+            # external sort
+            # two pointers
+            
         res = []
         i, j = 0, 0
         nums1.sort()
@@ -17,4 +36,3 @@ class Solution:
                 j += 1
                 
         return res
-        
