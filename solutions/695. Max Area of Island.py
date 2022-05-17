70871daad53f2dd853c023791dc1d8fb735e94f3
lumos! <43442549+LumosLiang@users.noreply.github.com>
2022-05-17 22:13:54 +0800

    218 ms

--- solutions/695. Max Area of Island.py
+++ solutions/695. Max Area of Island.py
@@ -1,6 +1,9 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        return self.DFS(grid)
         
+    def BFS(self, grid):
+        # loop all, and bfs all, DFS怎么统计cnt呢？
         
         res = 0
         m = len(grid)
@@ -16,7 +19,6 @@ class Solution:
                     q = collections.deque([[i,j]])
                     while q:
                         x,y = q.popleft()
-                        grid[x][y] = 0
                         area += 1
                         for l, k in [x, y + 1],[x, y - 1],[x - 1, y],[x + 1, y]:
                             if 0 <= l <= m - 1 and 0 <= k <= n - 1 and grid[l][k] == 1:
@@ -26,31 +28,37 @@ class Solution:
                     res = max(res, area)
                 
         return res
-​
-    def maxAreaOfIslandTLE(self, grid: List[List[int]]) -> int:
+    
+    def DFS(self, grid):
         
+        # update the area start from i, j for this connected component
+        # pre-order traversal
+        def DFS(i, j):
+            nonlocal area
+            nonlocal grid
+            nonlocal m
+            nonlocal n
+            
+            if not (0 <= i < m and 0 <= j < n and grid[i][j] == 1):
+                return
+            
+            area += 1
+            grid[i][j] = 0
+            
+            DFS(i - 1, j)
+            DFS(i + 1, j)
+            DFS(i, j - 1)
+            DFS(i, j + 1)  
         
         res = 0
-        m = len(grid)
-        n = len(grid[0])
+        m, n = len(grid), len(grid[0])
         
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == 1:
                     
-                    # grid[i][j] = 0
                     area = 0
-                    
-                    q = collections.deque([[i,j]])
-                    while q:
-                        x,y = q.popleft()
-                        grid[x][y] = 0
-                        area += 1
-                        for l, k in [x, y + 1],[x, y - 1],[x - 1, y],[x + 1, y]:
-                            if 0 <= l <= m - 1 and 0 <= k <= n - 1 and grid[l][k] == 1:
-                               
-                                q.append([l, k])
-                                
+                    DFS(i, j)         
                     res = max(res, area)
                 
         return res
