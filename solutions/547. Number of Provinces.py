e5ca9aa0c1d0dd1d89303cead655847c303ee727
lumos! <43442549+LumosLiang@users.noreply.github.com>
2022-05-18 11:03:26 +0800

    318 ms

--- solutions/547. Number of Provinces.py
+++ solutions/547. Number of Provinces.py
@@ -1,57 +1,33 @@
 class Solution:
-    def findCircleNum1(self, isConnected: List[List[int]]) -> int:
-        
-        # DFS & Backtrack
-        
-        l, w = len(isConnected[0]), len(isConnected)
-        
-        cnt = 0
-        
-        for i in range(l):
-            for j in range(w):
-                if isConnected[i][j] == 1:
-                    
-                    q = collections.deque([(i, j)])
-                    cnt += 1
-                    
-                    while q:
-                        x, y = q.popleft()
-                        isConnected[x][y] = 0
-                        
-                        for ny in range(0, l):
-                            if ny == y: continue
-                            if isConnected[x][ny] == 1:
-                                isConnected[x][ny] = 0
-                                if x != ny:
-                                    q.append((ny, x))
-        
-        return cnt
-​
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
         
-        # BFS
-        l, w = len(isConnected[0]), len(isConnected)
+        # essentially 连通量的问题，和计算岛屿数量是一样的。换了一个样子处理而已
+        # BFS/DFS
         
+#         for i in range(n):
+    
+#             2, BFS dq:
+#                 for every poped node, add all connections itself of this node to dq, mark itself as 0, set the other connection, and                      its mirror one to 0
+#             3, cnt + 1 
+        
+        l = len(isConnected)
         cnt = 0
         
         for i in range(l):
-            for j in range(w):
-                if isConnected[i][j] == 1:
-                    
-                    q = collections.deque([(i, j)])
-                    cnt += 1
-                    
-                    isConnected[i][j] = 0
-                    
-                    while q:
-                        x, y = q.popleft()
-                        
-                        for ny in range(0, l):
-                            if ny == y: continue
-                            if isConnected[x][ny] == 1:
-                                isConnected[x][ny] = 0
-                                if x != ny:
-                                    isConnected[ny][x] = 0
-                                    q.append((ny, x))
+            if isConnected[i][i]:
+                q = collections.deque([(i, i)])
+                cnt += 1
+                
+                while q:
+                    x, y = q.popleft()
+                    for ny in range(0, l):
+                        if isConnected[x][ny] == 1:
+                            isConnected[x][ny] = 0
+                            isConnected[ny][x] = 0
+                            isConnected[ny][ny] = 0
+                            q.append((ny, x))
         
         return cnt
+        
+        
+        
