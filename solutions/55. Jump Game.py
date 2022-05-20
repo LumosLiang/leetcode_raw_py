d9ba7927b60be1aee8f2d9b69d4728a067e46ec0
lumos! <43442549+LumosLiang@users.noreply.github.com>
2022-05-20 23:02:03 +0800

    N/A

--- solutions/55. Jump Game.py
+++ solutions/55. Jump Game.py
@@ -1,40 +1,43 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
+        return self.sol1(nums)
         
+    # DP
+    # dp[i] -> can reach the last at idx: i
+    # dp[i - 1] dp[i]
+    # TLE
+    def sol1(self, nums):
         l = len(nums)
-        dp = [0] * l
+        dp = [False] * l
+        dp[-1] = True
         
-        dp[0] = nums[0]
+        for i in range(l - 2, -1, -1):
+            if i + nums[i] >= l - 1:
+                dp[i] = True
+            else:
+                for j in range(1, nums[i] + 1):
+                    if dp[i + j]:
+                        dp[i] = True
+                        break
+        
+        return dp[0]
+        
+    
+    # greedy O(N), O(1)
+    def sol2(self, nums):
 ​
-        for i in range(1, l - 1):
-            
-            if dp[i - 1] < i: return False
-            
-            dp[i] = max(i + nums[i], dp[i - 1])
-            
-            if dp[i] >= l - 1: return True
-            
-        return dp[l - 2] >= l - 1
-    
-    
-    def canJumpWrong(self, nums: List[int]) -> bool:
+        # 1. at every step: 
+        #   if idx <= farest, update farest.
+        #   else break. return -1
         
-    
-        dp = [False] * len(nums)
+        farest = 0
         
-        if nums[0] == 0: 
-            if len(nums) == 1: return True
-            else: return False
-        else:
-            dp[0] = True
-            
-            for i in range(1, len(nums)):
-                temp = False
-                
-                for j in range(0, i):
-                    temp |= dp[j] and (nums[j] > (i - j - 1))
-                
-                if not temp: return False
-                dp[i] = temp
+        for i in range(len(nums)):
+            if i <= farest:
+                farest = max(farest, i + nums[i])
+                if farest >= len(nums) - 1:
+                    return True
+            else: 
+                return False
         
-        return dp[-1]
+        
