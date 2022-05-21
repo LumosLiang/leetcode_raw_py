e3d84baffc34308b234414e328eaa9b86a3e5e7f
lumos! <43442549+LumosLiang@users.noreply.github.com>
2022-05-21 18:00:09 +0800

    1417 ms

--- solutions/134. Gas Station.py
+++ solutions/134. Gas Station.py
@@ -1,27 +1,31 @@
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
-         
+        
+        # [1,3,6,2,1,6]
+        # [3,4,5,1,4,2]
+        
+        # insights
+        # 每一步的最优解就是最后的解
+        # 1. must start from diff > 0
+        # 2. if face any runsum < 0, then any station include and behind this station are excluded
+        
+        
+        diff = [gas[i] - cost[i] for i in range(len(gas))]
+        # diff += diff
         length = len(gas)
-        diff = [gas[i] - cost[i] for i in range(length)]
         
-        diff += diff
+        rsum = 0
+        l = 0
         
-        l, r = 0, 0 
-        
-        while r < 2 * length:
+        for r in range(length * 2):
+            rsum += diff[r % length]
             
-            if diff[r] >= 0:
-                temp, cnt = 0, 0
-                
-                while r < 2 * length and temp + diff[r] >= 0:
-                    temp += diff[r]
-                    cnt += 1
-                    if cnt == length:
-                        return l
-                    r += 1
-            r += 1
-            l = r
-        
+            if rsum < 0: 
+                l = r + 1
+                rsum = 0
+            
+            if r - l + 1 == length:
+                return l
+            
         return -1
-    
-         
+        
