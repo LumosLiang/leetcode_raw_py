4470999772742bc3c792525f14bb9ebee80e2db7
lumos! <43442549+LumosLiang@users.noreply.github.com>
2022-05-21 19:37:20 +0800

    Update 300. Longest Increasing Subsequence.py

--- solutions/300. Longest Increasing Subsequence.py
+++ solutions/300. Longest Increasing Subsequence.py
@@ -1,48 +1,99 @@
 class Solution:
-    def lengthOfLIS(self, nums: List[int]) -> int:
-        
-        # 这是一个什么问题？
-        
-        # DFS 问题: 每一个状态都有选择，穷举的想法是，穷举所有的组合，选择递增的，在这个过程找最大的递增序列。
-        # 一旦开始写memo，就进入了top-down DP的想法。
-        
-        memo = {}
-        def DFS(nums, idx):
-            
-            if idx == len(nums) - 1:
-                return 1
-            
-            if idx not in memo:
-                memo[idx] = 1
-                for i in range(idx + 1, len(nums)):
-                    if nums[i] > nums[idx]:
-                        memo[idx] = max(memo[idx], DFS(nums, i) + 1)
-                
-            return memo[idx]
-        
-        res = [DFS(nums, i) for i in range(len(nums))]
-        return max(res)
-    
-    def DP(self, nums):  
-        # DFS -> DP 问题，
-        # state: 对于DFS，dp[i + 1] 是 nums[i + 1:]的最长的递增序列是多少。
-        # dp[i] for j in i + 1 ~ len(nums) - 1, 如果nums[i] < nums[j], dp[i] = 1 + dp[j] 取最大的dp[i]
-        # base： dp[0] = 1
-        
-        # O(N * N), O(N)
-    
-        dp = [1] * len(nums)
-        
-        for i in range(len(nums) - 2, -1, -1):
-            for j in range(i + 1, len(nums)):
-                if nums[i] < nums[j]:
-                    dp[i] = max(dp[i], dp[j] + 1)
-        
-        return max(dp)
-    
-    
-    
-            
-        
-        
-        
+    def lengthOfLIS(self, nums: List[int]) -> int:
+    
+        return self.DP4(nums)
+    
+    def backtrack(self, nums):
+        memo = {}
+        def DFS(nums, idx):
+            
+            if idx == len(nums) - 1:
+                return 1
+            
+            if idx not in memo:
+                memo[idx] = 1
+                for i in range(idx + 1, len(nums)):
+                    if nums[i] > nums[idx]:
+                        memo[idx] = max(memo[idx], DFS(nums, i) + 1)
+                
+            return memo[idx]
+        
+        res = [DFS(nums, i) for i in range(len(nums))]
+        return max(res)
+    
+    # O(N^2), O(N)
+    def DP_forward(self, nums):
+        # dp[i]'s state: the LIS of the nums[:i + 1]
+        # function: dp[i + 1] = max(max([dp[j] + 1 for j in range(0, i) if nums[i + 1] > nums[j]]), 1)
+        # base: dp[0] = 1
+        
+        dp = [1] * len(nums)
+        
+        for i in range(1, len(nums)):
+            temp = [dp[j] + 1 for j in range(0, i) if nums[i] > nums[j]]
+            if temp:
+                dp[i] = max(temp)
+        
+        return max(dp)
+
+    def DP_backward(self, nums):  
+        # dp[i]'s state: the LIS of the nums[i:]
+        # function: dp[i - 1] = max(max([dp[j] + 1 for j in range(i, len(nums)) if nums[i - 1] < nums[j]]), 1)
+        # base： dp[-1] = 1
+        
+        # O(N * N), O(N)
+    
+        dp = [1] * len(nums)
+        
+        for i in range(len(nums) - 2, -1, -1):
+            for j in range(i + 1, len(nums)):
+                if nums[i] < nums[j]:
+                    dp[i] = max(dp[i], dp[j] + 1)
+        
+        return max(dp)
+    
+    # O(NlogN), O(N)
+    def DP3(self, nums):
+        
+        LIS = [nums[0]]
+        
+        for i in range(1, len(nums)):
+            if nums[i] > LIS[-1]:
+                LIS.append(nums[i])
+            else:
+                l, r = 0, len(LIS) - 1
+                while l < r:
+                    mid = (l + r) // 2
+                    if LIS[mid] < nums[i]:
+                        l = mid + 1
+                    else:
+                        r = mid
+                
+                LIS[l] = nums[i]
+            
+        return len(LIS) 
+
+        
+    # [10,9,2,5,1,7,101,18]
+    # with compute the actual LIS under O(N*N), O(N ^ 2)
+    def DP4(self, nums):
+        
+        dp = [[num] for num in nums]
+        
+        for i in range(1, len(nums)):
+            temp, longest = -1, []
+            for j in range(i):
+                if nums[j] < nums[i] and len(dp[j]) > temp:
+                    longest = dp[j]
+                    temp = len(dp[j])
+            if temp != -1:
+                dp[i] = longest + dp[i]
+            
+        max_len = 0
+        res = []
+        for i in range(len(dp)):
+            if len(dp[i]) > max_len:
+                res = dp[i]
+                max_len = len(dp[i])
+        
+        return len(res)
