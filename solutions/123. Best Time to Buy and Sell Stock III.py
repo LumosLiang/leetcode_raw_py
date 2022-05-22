5cb5c42113e347ee7892704f60802d8fd7478253
lumos! <43442549+LumosLiang@users.noreply.github.com>
2022-05-22 18:27:14 +0800

    3072 ms

---
+++ solutions/123. Best Time to Buy and Sell Stock III.py
@@ -0,0 +1,24 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        # 时间序列
+        
+        # DP maxprofit at different choice
+        
+        # i - 1  持有第一股， 卖出第一股， 持有第二股，卖出第二股
+        
+        # i      持有第一股， 卖出第一股， 持有第二股，卖出第二股
+        
+        
+        dp = [[0 for _ in range(4)] for _ in range(len(prices))]
+        
+        dp[0][0], dp[0][1], dp[0][2], dp[0][3] = -prices[0], 0, -prices[0], 0
+    
+        for i in range(1, len(prices)):
+            dp[i][0] = max(dp[i - 1][0], -prices[i])
+            dp[i][1] = max(dp[i - 1][0] + prices[i], dp[i - 1][1])
+            
+            dp[i][2] = max(dp[i - 1][1] - prices[i], dp[i - 1][2])
+            dp[i][3] = max(dp[i - 1][2] + prices[i], dp[i - 1][3])
+        
+        print(dp)
+        return max(dp[-1])
