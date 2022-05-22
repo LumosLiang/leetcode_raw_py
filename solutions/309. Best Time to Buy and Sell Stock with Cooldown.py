5f6de9f89ccfb449d044ae827aa43221a9e0463a
lumos! <43442549+LumosLiang@users.noreply.github.com>
2022-05-22 21:54:23 +0800

    85 ms

--- solutions/309. Best Time to Buy and Sell Stock with Cooldown.py
+++ solutions/309. Best Time to Buy and Sell Stock with Cooldown.py
@@ -12,7 +12,7 @@ class Solution:
         dp[0][0], dp[0][1], dp[0][2] = -prices[0], 0, 0
         
         for i in range(1, len(prices)):
-            dp[i][0] = max(dp[i - 1][0], dp[i - 1][2] - prices[i])
+            dp[i][0] = max(dp[i - 1][0], dp[i - 1][2] - prices[i], -prices[i])
             dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] + prices[i])
             dp[i][2] = max(dp[i - 1][2], dp[i - 1][1])
         
