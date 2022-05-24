ae670ce8e7360cf15ef77b34e3f72f817fc43c28
lumos! <43442549+LumosLiang@users.noreply.github.com>
2022-05-24 21:52:48 +0800

    824 ms

--- solutions/11. Container With Most Water.py
+++ solutions/11. Container With Most Water.py
@@ -1,22 +1,33 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
+        
+        # 穷举 O(N * N) -> TLE
+        
+        # area = (i - j) * min(h[i], h[j])
+        
+        # [1,8,6,2,5,4,8,3,7]
+        
+        # min(h[i], h[j]) 尽力的去提高这一部分。不用去管结果一定是否高，但是只要有可能我们就去试
+        
+        # 双指针的做法
+        # Greedy
+        l, r = 0, len(height) - 1
         res = 0
         
-        l = 0
-        r = len(height) - 1
-        
         while l <= r:
-            v = min(height[l], height[r]) * (r - l)
-            if v > res: res = v
-            # move the curr lower height to a place where first reach the higher height
-            temp = 0
-            if height[l] <= height[r]:
-                while l + temp <= r and height[l + temp] <= height[l]:
-                    temp += 1
-                l += temp
-            elif height[l] > height[r]:
-                while r - temp >= l and height[r - temp] <= height[r]:
-                    temp += 1
-                r -= temp
+            
+            min_height = min(height[l], height[r])
+            res = max(res, (r - l) * min_height)
+            
+            # 要去提高的地方在哪一侧
+            
+            if min_height == height[l]:
+                while l + 1 <= r and height[l + 1] < height[l]:
+                    l += 1
+                l += 1
+            else:
+                while l <= r - 1 and height[r - 1] < height[r]:
+                    r -= 1
+                r -= 1
+            
         return res
-                
