9ad49587a73c5e12276d262e4b7bed175926fa97
lumos! <43442549+LumosLiang@users.noreply.github.com>
2022-05-25 17:37:50 +0800

    198 ms

--- solutions/438. Find All Anagrams in a String.py
+++ solutions/438. Find All Anagrams in a String.py
@@ -1,31 +1,45 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        
-        
-        # need: p's anagram
-        # window: length is the same and frequecy is the same.
-        # where to update, as long as we find the one
-        
-        
+​
+    # anagrams: keys are same, values are same
+    
+    # 明确这是一个sliding window问题
+    # 单调性体现在：一旦这些字母能够被囊括，那么剩下的事情就是我要在这个区间进行搜索。搜到可以一个区间，
+    # 在这个区间中，字母一样，频率一样。这就是答案，然后left + 1
+    # 我们需要什么
+    # p的key，和freq。window的key和frequ，进入和跳出区间的条件
+    
+        window = collections.Counter()
+        need = collections.Counter(p)
+        l = 0
         res = []
-        window, need = collections.Counter(), collections.Counter(p)
-        l, length, count = 0, len(p), 0
-        
+        match_cnt = 0
+​
         for r, val in enumerate(s):
             window[val] += 1
+​
             if val in need and window[val] == need[val]:
-                count += 1
+                match_cnt += 1
+​
+            # 当前的window一定包含所有的key，且每一个key的个数都会至少等于need中对应的个数
+            
+            while match_cnt == len(need):
                 
-            if r - l + 1 == length:
-                if count == len(need): 
-                    res.append(l)
-                
-                if s[l] in need and window[s[l]] == need[s[l]]:
-                    count -= 1
-                window[s[l]] -= 1
-                l += 1
+                # if r - l + 1 == len(p):
+                if len(window) == len(need) and all([window[key] == need[key] for key in window]):
+                    res.append(l)    
                     
+                window[s[l]] -= 1
+                
+                if s[l] in need and window[s[l]] < need[s[l]]:
+                    match_cnt -= 1
+​
+                if window[s[l]] == 0:
+                    del window[s[l]]
+​
+                l += 1
+                
+​
         return res
-        
- 
-        
+    
+    
