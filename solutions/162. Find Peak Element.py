a6fec34df3951a08c9cd1c2d7f994f82192317ca
lumos! <43442549+LumosLiang@users.noreply.github.com>
2022-05-26 23:25:57 +0800

    53 ms

--- solutions/162. Find Peak Element.py
+++ solutions/162. Find Peak Element.py
@@ -1,18 +1,41 @@
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
         
-        l, r = 0, len(nums) - 1
+        return self.sol2(nums)
+​
+    def sol1(nums):
+        lo, hi = 0, len(nums) - 1
         
-        # 还是划分到自己的区间内，
-        # 斜率比较，还是要观察这种insight
+        while lo < hi:
+            mid = lo + (hi - lo) // 2
+​
+            if hi - lo == 1:
+                if nums[mid] < nums[mid + 1]:return mid + 1
+                else: return mid
+            else:
+                if nums[mid - 1] < nums[mid] and nums[mid] > nums[mid + 1]: return mid
+                elif nums[mid - 1] < nums[mid]: lo = mid
+                elif nums[mid] > nums[mid + 1]: hi = mid
+                else: lo = mid 
         
-        while l < r:
-            mid = (l + r) // 2
+        return hi
+    
+    def sol2(self, nums):
+        
+        # 归到一个单调区间上，然后再做
+        
+        lo, hi = 0, len(nums) - 1
+        
+        while lo < hi:
+            
+            mid = (hi + lo) // 2
+​
+#             if mid > 0 and nums[mid] > nums[mid + 1] and nums[mid] > nums[mid - 1]:
+#                 return mid
             
             if nums[mid] < nums[mid + 1]:
-                l = mid + 1
+                lo = mid + 1
             else:
-                r = mid
+                hi = mid
         
-        return l
-            
+        return lo
