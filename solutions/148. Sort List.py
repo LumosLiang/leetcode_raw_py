8fb5a77bcf37fab07bc5fd085b50e78d0bc914d7
lumos! <43442549+LumosLiang@users.noreply.github.com>
2022-05-26 08:14:17 +0800

    Update 148. Sort List.py

--- solutions/148. Sort List.py
+++ solutions/148. Sort List.py
@@ -1,52 +1,71 @@
 # Definition for singly-linked list.
 # class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-​
-​
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+
 class Solution:
-    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        
-        # merge sort -> 
-​
-            # preorder traversal
-            # first locate mid
-            # then DC
-            # then merge
-​
-        # iterative merge sort
-            # set a k, and increase k at the appro time
-            # merge first k, then next k
-            # merge those k
-        
-        # O(NlogN), O(logN)
-        # quick sort
-        # pick the first as one as pivot
-​
-        if not head or head.next == None: return head
-​
-        dummy, small, large = ListNode(), ListNode(), ListNode()
-        pivot = head.val
-        d, s, l, curr = dummy, small, large, head.next
-​
-        while curr:
-            if curr.val < pivot:
-                s.next = curr
-                s = s.next
-            else:
-                l.next = curr
-                l = l.next
-            curr = curr.next
-​
-        s.next = None
-        l.next = None
-​
-        left = self.sortList(small.next)
-        right = self.sortList(large.next)
-​
-        if left:
-            currl = left
-            while currl.next: currl = currl.next
-            currl.next = head
-            head.next = right
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        return self.quicksort(head)
+    
+    # O(NlogN), O(logN)
+    # quick sort
+    
+    def quicksort(self, head):
+        
+        if not head or not head.next: return head
+        
+        # pick pivot, randomly
+        length = 0
+        curr = head
+        h = []
+        while curr:
+            h.append(curr) 
+            length += 1
+            curr = curr.next
+        pivot_node_idx = random.randint(0, length - 1)
+        pivot_node = h[pivot_node_idx]
+        pivot_val = pivot_node.val
+
+        # generate linked list < pivot, = pivot, and > pivot
+        l, r, m = ListNode(), ListNode(), ListNode()
+        curr, lcurr, rcurr, mcurr = head, l, r, m
+        
+        while curr:
+            if curr.val < pivot_val:
+                lcurr.next = curr
+                lcurr = lcurr.next
+            elif curr.val > pivot_val:
+                rcurr.next = curr
+                rcurr = rcurr.next
+            else:
+                mcurr.next = curr
+                mcurr = mcurr.next
+            curr = curr.next
+    
+        # processing node.
+        mcurr.next = None
+        lcurr.next = None
+        rcurr.next = None
+        
+        # divide and conquer
+        left = self.quicksort(l.next)
+        right = self.quicksort(r.next)
+        
+        # link them all
+        mcurr.next = right
+        if not left: return m.next
+
+        left_end = left
+        while left_end.next: left_end = left_end.next
+        left_end.next = m.next
+        return left
+
+    def merge_sort(self, head):
+        
+#         fast/slow -> mid
+#         mergesort(left)
+#         mergesort(right)
+        
+#         return merge(left, right)
