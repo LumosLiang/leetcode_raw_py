03dd007e894bac4f3df30a49181430c0a4104b92
lumos! <43442549+LumosLiang@users.noreply.github.com>
2022-06-03 10:58:32 +0800

    Update 202. Happy Number.py

--- solutions/202. Happy Number.py
+++ solutions/202. Happy Number.py
@@ -1,30 +1,49 @@
 class Solution:
-    def isHappy(self, n: int) -> bool:
-        
-        # 2 -> 4 -> 16 -> 36 + 1 -> 9 + 49 58 -> 25 + 64 89-> 64 + 81 145-> 1 + 16 + 25 42 -> 16 + 4 20 -> 4   
-        # Non-happy的数字 会在一个循环内
-        
-        
-        def sum_of_square_digit(num):
-            
-            res = 0
-            while num:
-                num, digit = num // 10, num % 10
-                res += digit ** 2
-            
-            return res
-​
-        s = set()
-        s.add(n)
-        
-        while True:
-            curr_val = sum_of_square_digit(n)
-            if curr_val == 1:
-                return True
-            elif curr_val in s:
-                return False
-            else:
-                s.add(curr_val)
-                n = curr_val
-        
-                
+    def isHappy(self, n: int) -> bool:
+        return self.sol2(n)
+    
+    # 
+    def sol1(self, n):
+        
+        def sum_of_square_digit(num):
+            
+            res = 0
+            while num:
+                num, digit = num // 10, num % 10
+                res += digit ** 2
+            
+            return res
+
+        s = set()
+        s.add(n)
+        
+        while True:
+            curr_val = sum_of_square_digit(n)
+            if curr_val == 1:
+                return True
+            elif curr_val in s:
+                return False
+            else:
+                s.add(curr_val)
+                n = curr_val
+                
+    # slow, fast pointer.    
+    def sol2(self, n):
+        
+        def helper(num):
+            res = 0
+            while num:
+                num, digit = num // 10, num % 10
+                res += digit ** 2
+            
+            return res
+
+        slow, fast = n, n
+        
+        while True:
+            
+            slow = helper(slow)
+            fast = helper(helper(fast))
+            
+            if fast == 1: return True
+            if slow == fast: return False
