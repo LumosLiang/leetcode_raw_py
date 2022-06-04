4cbb2ecaacd95372c2d6c02abe8a402c087a2b5e
lumos! <43442549+LumosLiang@users.noreply.github.com>
2022-06-05 00:08:12 +0800

    119 ms

--- solutions/23. Merge k Sorted Lists.py
+++ solutions/23. Merge k Sorted Lists.py
@@ -5,38 +5,75 @@
 #         self.next = next
 ​
 class Solution:
-    def mergeKLists(self, lists: List[ListNode]) -> ListNode:
-​
-        if not lists: return 
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        # return self.merge_sort(lists, 0, len(lists) - 1)
         
-        if len(lists) == 1: return lists[0]
+        return self.heap(lists)
         
-        lo, hi = 0, len(lists) - 1
-        mid = lo + (hi - lo) // 2
-        left = self.mergeKLists(lists[:mid + 1])
-        right = self.mergeKLists(lists[mid + 1:])
+    # O(logK) * O(n1 + n2)
+    def merge_sort(self, lists, l, r):
+        
+        # base
+        if l > r: return None
+        if l == r: return lists[l]
+        
+        mid = (l + r) // 2
+        
+        left = self.mergeHelper(lists, l, mid)
+        right = self.mergeHelper(lists, mid + 1, r)
         
         return self.merge(left, right)
+    
+    # O(n1 + n2)
+    def merge(self, h1, h2):
         
+        if not h1 or not h2: return h1 or h2
         
-    def merge(self, l1, l2):
+        dummy = ListNode()
+        c1, c2, curr = h1, h2, dummy
         
-        if not l1: return l2
-        if not l2: return l1
-        
-        dummy = ListNode(-1)
-        curr1, curr2, curr3 = l1, l2, dummy
-        
-        while curr1 and curr2:
-            if curr1.val >= curr2.val:
-                curr3.next = curr2
-                curr2 = curr2.next
+        while c1 and c2:
+            if c1.val < c2.val:
+                curr.next = c1
+                c1 = c1.next
             else:
-                curr3.next = curr1
-                curr1 = curr1.next
-            curr3 = curr3.next
-        
-        curr3.next = curr2 if not curr1 else curr1
+                curr.next = c2
+                c2 = c2.next
+            curr = curr.next
+            
+        curr.next = c1 if c1 else c2
         
         return dummy.next
     
+    # [
+    #   1->4->5,
+    #   1->3->4,
+    #   2->6
+    # ]
+    
+    # 需要注意的点是：如何制造唯一性，且能让heap做出比较
+    # python中heap的比价逻辑，比0，然后比1，然后比2
+    
+    def heap(self, lists):
+        heap = []
+        for i in range(len(lists)):
+            if lists[i]:
+                heapq.heappush(heap, (lists[i].val, i, lists[i]))
+        
+        dummy = ListNode()
+        curr = dummy
+        
+        while heap:
+            val, idx, nxt = heapq.heappop(heap)
+            if nxt.next:
+                heapq.heappush(heap, (nxt.next.val, idx, nxt.next))
+            
+            curr.next = nxt
+            curr = curr.next
+            
+        return dummy.next
+        
+        
+        
+        
+        
