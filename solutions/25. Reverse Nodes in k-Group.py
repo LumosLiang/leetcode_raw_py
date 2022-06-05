686090842e614072483ae1b43f1f5ad9b130d807
lumos! <43442549+LumosLiang@users.noreply.github.com>
2022-06-05 13:42:01 +0800

    51 ms

--- solutions/25. Reverse Nodes in k-Group.py
+++ solutions/25. Reverse Nodes in k-Group.py
@@ -5,10 +5,10 @@
 #         self.next = next
 ​
 class Solution:
-    def reverseKGroup(self, head, k):
-        
-        return self.recursive(head,k)
+    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
 ​
+        return self.iterative(head, k)
+    
     # O(N), O(N//k)
     def recursive(self, head, k):
         if not head: return None
@@ -30,43 +30,40 @@ class Solution:
         
         return nh
     
-    # O(k), O(1)
-    def reverse(self, head):
+    # follow-up1: require to reverse even if the number of nodes is not a multiple of k
+    def followup1(self, head, k):
+        
         if not head: return None
         
-        pre, curr, nxt = None, head, None
+        curr, cnt = head, 0
         
-        while curr:
-            nxt = curr.next
-            curr.next = pre
-            pre = curr
-            curr = nxt
+        while curr and cnt < k - 1:
+            curr = curr.next
+            cnt += 1
         
-        return pre
+        if not curr: return self.reverse(head)
+        
+        nxt = curr.next
+        curr.next = None
     
-    # O(N), O(1)
-​
-    # dummy
-​
-    # 每一次我都取n, n+k的node， 
-    # start, trail
-    # 但是同时，要记录，该段翻转的前一个点和后一个点，pre, tnxt.
-    # in-place reverse: 注意的点就是要，确定好新的 head, 和tail
-​
-    # start.pre.next = trail.
-    # start.next = trail.next
-    # 再次记录头
-​
-    # 取下一组K，
-​
-    # 如果取不到，就直接return dummy.next
-​
-    # 如果题目要求是 无论不足也翻转的话，那这种事情，应该只出现一次，就是在最后一组。
-    # 如果遇到取不到k，
-    # 就也翻转一次。
-​
-    def iterative(self, head, k):
+        new_nxt = self.followup1(nxt, k)
+        nh = self.reverse(head)
+        head.next = new_nxt
         
-        pass
+        return nh
+    
+    # follow-up2, do this from the tail
+    def followup2(self, head, k):
+        if not head: return None
         
-            
+        def reversebykgroup(head, k):
+            if not head: return None
+    
+            cnt = 0
+            curr = head
+​
+            while cnt < k - 1:
+                cnt += 1
+                curr = curr.next
+​
+            nxt = curr.next
