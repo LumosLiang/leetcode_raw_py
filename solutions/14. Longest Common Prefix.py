b03cfaf64a5d06f89ef9084a7399a0066d726398
lumos! <43442549+LumosLiang@users.noreply.github.com>
2022-06-09 18:20:39 +0800

    49 ms

--- solutions/14. Longest Common Prefix.py
+++ solutions/14. Longest Common Prefix.py
@@ -1,22 +1,55 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
         
-        if len(strs) == 1: return strs[0]
-        
-        res, p = "", 0
-        
-        while (self.isequal(strs, p)):
-            res += strs[0][p]
-            p += 1
-        
-        return res
+        return self.sol1(strs)
     
-    def isequal(self, strs, p):
+    # one by one
+    def sol1(self, strs):
         
-        res = True
-        
-        for i in range(len(strs) - 1):
-            if strs[i][p:] and strs[i + 1][p:] and strs[i][p] == strs[i + 1][p]: continue
-            else: return False
+        def get_common(s1, s2):
+            p1, p2 = 0, 0
+​
+            while p1 < len(s1) and p2 < len(s2):
+                if s1[p1] != s2[p2]:
+                    return s1[:p1]
+                p1 += 1
+                p2 += 1
+​
+            return s1 if p1 == len(s1) else s2
+    
+        curr = strs[0]
+        for i in range(1, len(strs)):
+            nxt = strs[i]
+            curr = get_common(curr, nxt)
             
-        return res
+        return curr
+        
+    
+    # O(wordleng * logN)
+    # divide and conquer
+    def sol2(self, strs):
+        
+        def helper(strs, left, right):
+            
+            if left == right: return strs[left]
+            
+            def get_common(s1, s2):
+                
+                p1, p2 = 0, 0
+                
+                while p1 < len(s1) and p2 < len(s2):
+                    if s1[p1] != s2[p2]:
+                        return s1[:p1]
+                    p1 += 1
+                    p2 += 1
+                
+                return s1 if p1 == len(s1) else s2
+                
+            mid = (left + right) // 2
+            
+            l_LCP = helper(strs, left, mid)
+            r_LCP = helper(strs, mid + 1, right)
+            
+            return get_common(l_LCP, r_LCP)
+        
+        return helper(strs, 0, len(strs) - 1)
