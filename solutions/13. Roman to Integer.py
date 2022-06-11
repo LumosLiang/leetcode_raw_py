c9b5b9d24dd45b7d0dc43b6ef9dfdf0ec36b23f9
lumos! <43442549+LumosLiang@users.noreply.github.com>
2022-06-11 14:36:11 +0800

    96 ms

--- solutions/13. Roman to Integer.py
+++ solutions/13. Roman to Integer.py
@@ -1,19 +1,45 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        
-        t = {'I':1, 'V':5, 'X':10, 'L': 50, 'C':100, 'D':500, 'M':1000}
+         
+        return self.sol2(s)
+    # with exception
+    def sol1(self, s):    
+        hash = {'I':1, 'IV':4, 'V':5, 'IX':9, 'X':10, 'XL':40, 'L': 50, 'XC':90, 'C':100, 'CD':400, 'D':500, 'CM':900, 'M':1000}
 ​
-        S, over = 0, 0
         
-        for i in range(len(s) - 1):
-            pre = t[s[i]]
-            lat = t[s[i + 1]]
-            if pre < lat:
-                over += 2 * pre
-            S += pre
+        res, p = 0, 0
         
-        return S + t[s[-1]] - over
+        while p < len(s) - 1:
+            k = s[p:p + 2] 
+            if k in hash:
+                res += hash[k]
+                p += 2
+            else:
+                res += hash[s[p]]
+                p += 1
             
+        return res if p == len(s) else (res + hash[s[p]])
+        
+        
+    # with out exception
+    def sol2(self, s):
+        
+        hash = {'I':1, 'V':5, 'X':10, 'L': 50, 'C':100, 'D':500, 'M':1000}
+    
+#         MCMXCIV
+                
+#         1000 + (-100) + 1000 + (-10) + 100 + (-1) + 5 = 
+                
+        res, p = 0, 0
+                
+        while p < len(s) - 1:
+            curr, nxt = hash[s[p]], hash[s[p + 1]]
+            if curr >= nxt:
+                res += curr
+            else:
+                res -= curr
+            p += 1
+        
+        return res + hash[s[p]]
             
         
-            
