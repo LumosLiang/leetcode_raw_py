8b57df6344017af9c69ae1eeccea1adc0cffc10e
lumos! <43442549+LumosLiang@users.noreply.github.com>
2022-06-12 13:13:16 +0800

    111 ms

---
+++ solutions/459. Repeated Substring Pattern.py
@@ -0,0 +1,17 @@
+class Solution:
+    def repeatedSubstringPattern(self, s: str) -> bool:
+        
+        pre, l = 0, len(s)
+        prefix = [0] * l
+        
+        for post in range(1, l):
+            
+            while pre > 0 and s[pre] != s[post]:
+                pre = prefix[pre - 1]
+            if s[pre] == s[post]:
+                pre += 1
+            prefix[post] = pre
+        
+        if prefix[l - 1] != 0 and not l % (l - prefix[l - 1]):
+            return True
+        return False
