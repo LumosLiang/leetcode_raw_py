7694adb29f4ed96159082452e02ac89a02f02444
lumos! <43442549+LumosLiang@users.noreply.github.com>
2022-06-20 15:53:07 +0800

    407 ms

--- solutions/238. Product of Array Except Self.py
+++ solutions/238. Product of Array Except Self.py
@@ -1,35 +1,35 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         
-#         [1,2,3,4]
-#         1, 2, 6, 24
-#         24 24 12  4
+        #         [1,2,3,4]
+        #         1, 2, 6, 24
+        #         24 24 12  4
+        #        思路变化： 1, 比较直接：分别算每一位的前缀积，后缀积（包含自己） -> 2, 分别算每一位的前后缀积, 不包含自己， -> 3, 一块儿算。
         
-        return self.sol2(nums)
+        return self.sol1(nums)
     
     # O(N), O(N)
     def sol1(self, nums):
         
         l = len(nums)
-        
-        pre = [1] * (l + 1)
-        for i in range(l):
-            pre[i + 1] = pre[i] * nums[i]
-        
-        post = [1] * (l + 1)
-        for i in range(l - 1, -1, -1):
-            post[i] = post[i + 1] * nums[i]
-        
+        # 直接算每一位的前后缀积
+        pre = [1] * l
+        for i in range(1, l):
+            pre[i] = pre[i - 1] * nums[i - 1]
+        print(pre)
+        post = [1] * l
+        for i in range(l - 2, -1, -1):
+            post[i] = post[i + 1] * nums[i + 1]
+        print(post)
         res = [1] * l
         for i in range(l):
-            res[i] = pre[i] * post[i + 1]
+            res[i] = pre[i] * post[i]
         
         return res
         
     # O(N), O(1)
     # [1,2,3,4,5,6]
-    # in-place的思想
-    
+    # 改进了以上的算法，放在一个里面算。
     def sol2(self, nums):
         
         res = [1] * len(nums)
