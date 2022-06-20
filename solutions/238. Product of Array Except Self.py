55530d335d7fad6854acc06b323340ef7aef7b98
lumos! <43442549+LumosLiang@users.noreply.github.com>
2022-06-20 15:44:26 +0800

    407 ms

--- solutions/238. Product of Array Except Self.py
+++ solutions/238. Product of Array Except Self.py
@@ -1,16 +1,47 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         
-        ans = [1] * len(nums)
+#         [1,2,3,4]
+#         1, 2, 6, 24
+#         24 24 12  4
         
-        left, right = 1, 1
+        return self.sol2(nums)
+    
+    # O(N), O(N)
+    def sol1(self, nums):
         
+        l = len(nums)
+        
+        pre = [1] * (l + 1)
+        for i in range(l):
+            pre[i + 1] = pre[i] * nums[i]
+        
+        post = [1] * (l + 1)
+        for i in range(l - 1, -1, -1):
+            post[i] = post[i + 1] * nums[i]
+        
+        res = [1] * l
+        for i in range(l):
+            res[i] = pre[i] * post[i + 1]
+        
+        return res
+        
+    # O(N), O(1)
+    # [1,2,3,4,5,6]
+    # in-place的思想
+    
+    def sol2(self, nums):
+        
+        res = [1] * len(nums)
+        
+        pre_P, post_P = 1, 1
         for i in range(len(nums)):
-            ans[i] *= left
-            ans[-1 - i] *= right 
-            left *= nums[i]
-            right *= nums[-1 - i]
-        
-        return ans
+            res[i] *= pre_P   
+            pre_P *= nums[i]
+        #[1,1,2,6]
+        for i in range(len(nums) - 1, -1, -1):
+            res[i] *= post_P
+            post_P *= nums[i]
+        # [24, 12, 4, 1]
+        return res
             
-        
