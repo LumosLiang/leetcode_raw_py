598e3ec4b2f71c107db6ddba6c07aaf1bb1b72b5
lumos! <43442549+LumosLiang@users.noreply.github.com>
2022-06-20 16:36:42 +0800

    Update 424. Longest Repeating Character Replacement.py

--- solutions/424. Longest Repeating Character Replacement.py
+++ solutions/424. Longest Repeating Character Replacement.py
@@ -14,3 +14,23 @@ class Solution:
         return res
         
         
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        
+    # 本质，我已经有的长度 - 最多的element <= k
+        
+        start = 0
+        res = 0
+        maxf = 0
+        
+        window = collections.Counter()
+        
+        for end, val in enumerate(s):
+            window[val] += 1
+            maxf = max(maxf, window[val])
+            while end - start + 1 - maxf > k:
+                window[s[start]] -= 1
+                start += 1
+            res = max(res, end - start + 1)
+            
+        return res
