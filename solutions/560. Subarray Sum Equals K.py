0d15014da32259f3af4929227c4bf775184abf12
lumos! <43442549+LumosLiang@users.noreply.github.com>
2022-06-23 10:20:25 +0800

    671 ms

--- solutions/560. Subarray Sum Equals K.py
+++ solutions/560. Subarray Sum Equals K.py
@@ -1,27 +1,22 @@
 class Solution:
-    def subarraySum(self, nums, k):
+    def subarraySum(self, nums: List[int], k: int) -> int:
         
-        prefix_sum = [0]
+        prefix = [0] * (len(nums) + 1) 
         
         for i in range(len(nums)):
-            prefix_sum.append(nums[i] + prefix_sum[i])
-        
-        hash, res = {k:1}, 0
-        
-        # hashtable {key, val}
-        # key is the relative distance = k, val is the count of relative distance = k
-        
-        for i in range(1, len(prefix_sum)):
-            if prefix_sum[i] in hash:
-                res += hash[prefix_sum[i]]
+            prefix[i + 1] = prefix[i] + nums[i]
             
-            if prefix_sum[i] + k in hash:
-                hash[prefix_sum[i] + k] += 1
-            else:
-                hash[prefix_sum[i] + k] = 1
+        hash = collections.Counter()
+        
+        res = 0
+        for val in prefix:
+            if val in hash:
+                res += hash[val]        
+            hash[val + k] += 1
             
         return res
-        
-        
-        
+            
+            
                 
+        
+        
