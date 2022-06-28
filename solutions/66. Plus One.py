ee419243b371f5a34ce272503065df473a636bee
lumos! <43442549+LumosLiang@users.noreply.github.com>
2022-06-28 18:39:04 +0800

    54 ms

--- solutions/66. Plus One.py
+++ solutions/66. Plus One.py
@@ -1,3 +1,19 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
         
+        carry = 1
+        
+        for i in range(len(digits) - 1, -1, -1):
+            
+            curr = digits[i] + 1
+            digits[i] = curr % 10
+            carry = curr // 10
+            if carry == 0:
+                break
+​
+        if carry:
+            return [carry] + digits
+        return digits
+        
+        
+            
