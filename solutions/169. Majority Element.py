b5d6433d25e5d45b61a946f836054a861539bd23
lumos! <43442549+LumosLiang@users.noreply.github.com>
2022-07-05 22:35:38 +0800

    190 ms

--- solutions/169. Majority Element.py
+++ solutions/169. Majority Element.py
@@ -1,32 +1,48 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
+        return self.sol1(nums)
         
-    
-    
-    
     # O(N), O(1)
     # moore voting
-    def majorityElement(self, nums: List[int]) -> int:
-        winner = None
+    def sol1(self, nums: List[int]):
+        winner = none
         winner_cnt = 0
         
         for num in nums:
-            if winner is not None:
+            if winner is not none:
                 if num == winner:
                     winner_cnt += 1
                 else:
                     winner_cnt -= 1
                     if winner_cnt == 0:
-                        winner = None
+                        winner = none
             else:
                 winner = num
                 winner_cnt += 1
         
         return winner
     
-    # Sorting O(NlogN)
-    def majorityElement(self, nums):
+    def sol2(self, nums):
+        major = nums[0]
+        major_cnt = 1
+​
+        for i in range(1, len(nums)):
+​
+            if nums[i] == major:
+                major_cnt += 1        
+            else:
+                major_cnt -= 1
+                if major_cnt == -1:
+                    major = nums[i]
+                    major_cnt = 1
+​
+        return major
+    
+    # sorting o(nlogn)
+    def majorityelement2(self, nums):
         nums.sort()
         return nums[len(nums)//2]
                 
-                
+    
+​
+        
