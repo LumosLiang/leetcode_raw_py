0669859e2f4ac78781229b3aa25785e817eab0f1
lumos! <43442549+LumosLiang@users.noreply.github.com>
2022-07-05 17:03:31 +0800

    124 ms

--- solutions/1094. Car Pooling.py
+++ solutions/1094. Car Pooling.py
@@ -3,7 +3,9 @@ class Solution:
         
         # (2) 1 ---- 5
         #     (3) 3 ---- 7
-        
+        return self.sol2(trips, capacity)
+    
+    def sol1(self, trips, capacity):
         h = collections.defaultdict(int)
         
         for trip in trips:
@@ -18,5 +20,28 @@ class Solution:
         
         return True
         
+    def sol2(self, trips, capacity):
+        
+        heap = []
+        
+        trips.sort(key = lambda x: x[1])
+        
+        heapq.heappush(heap, (trips[0][2], trips[0][1], trips[0][0]))
+        people_cnt = trips[0][0]
+        
+        if people_cnt > capacity: return False
+    
+        for i in range(1, len(trips)):
             
+            while heap and trips[i][1] >= heap[0][0]:
+                end, start, num = heapq.heappop(heap)
+                people_cnt -= num
+                
+            heapq.heappush(heap, (trips[i][2], trips[i][1], trips[i][0]))
+            people_cnt += trips[i][0]
+​
+            if people_cnt > capacity:
+                return False
+         
+        return True
         
