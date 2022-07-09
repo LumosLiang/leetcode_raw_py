c795a738fcd2d3d4be39d310ade61d26097c6383
lumos! <43442549+LumosLiang@users.noreply.github.com>
2022-07-09 17:09:42 +0800

    45 ms

--- solutions/33. Search in Rotated Sorted Array.py
+++ solutions/33. Search in Rotated Sorted Array.py
@@ -1,35 +1,50 @@
 class Solution:
-    def search(self, nums: List[int], target: int) -> int:
-        
+    def search(self, nums, target):
+    
+        return self.sol2(nums, target)
+    
+    def sol1(self, nums):
         l, r = 0, len(nums) - 1
-        
-        # [6,7,0,1,2,3,4,5]
-        # [3,4,5,6,7,0,1,2]
-        # decide which part mid locate
-        # decide which part target locate
-        
-        # 先归到一个确定的区间内。
         
         while l <= r:
             mid = (l + r) // 2
             
-            # for 更快
             if nums[mid] == target: return mid
             
-            # 归区间
-            if nums[mid] > nums[r] and target <= nums[r]:
-                l = mid + 1
-            elif nums[mid] < nums[r] and target > nums[r]:
-                r = mid - 1
+            if nums[mid] > target:
+                if target >= nums[0] or nums[mid] <= nums[-1]:
+                    r = mid - 1
+                else:
+                    l = mid + 1
             else:
-                # 做二分
-                if nums[mid] == target:
-                    return mid
-                elif nums[mid] < target:
+                if target <= nums[-1] or nums[mid] >= nums[0]:
                     l = mid + 1
                 else:
                     r = mid - 1
-            
+    
         return -1
-    
-    
+​
+    def sol2(self, nums, target):
+        # 先归区间，再做二分
+        
+        l, r = 0, len(nums) - 1
+        
+        while l < r:
+            
+            mid = l + (r - l) // 2
+            
+            if nums[mid] == target: return mid
+            
+            if nums[mid] > nums[r] and target <= nums[r]:
+                l = mid + 1
+            elif nums[mid] <= nums[r] and target > nums[r]:
+                r = mid - 1
+            else:
+                if nums[mid] < target:
+                    l = mid + 1
+                else:
+                    r = mid
+        
+        return l if nums[l] == target else -1
+                        
+            
