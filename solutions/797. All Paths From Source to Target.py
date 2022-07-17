8cbf117d792c1ad652d255e848f4e61a5c38b2e5
lumos! <43442549+LumosLiang@users.noreply.github.com>
2022-07-17 18:29:22 +0800

    173 ms

--- solutions/797. All Paths From Source to Target.py
+++ solutions/797. All Paths From Source to Target.py
@@ -1,16 +1,51 @@
 class Solution:
-    def allPathsSourceTarget(self, graph):
+    def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+        
+#         dfs/bfs 都可以
+#         0 
+        
+#         0,4   0,3,   0,1
+        
+#               0,3,4  0,1,3  0,1,2  0,1,4
+                     
+#                      0,1,3,4   0,1,2,3
+                    
+#                                0,1,2,3,4
+​
+        return self.bfs(graph)
+​
+    def dfs(self, graph):↔​
+    
+    def bfs(self, graph):
+        
+        node_q = collections.deque([0])        
+        path_q = collections.deque([[0]])
         res = []
         
-        def backtrack(choices, choice, path):
-            if choice == len(choices)-1:
-                res.append(path[:])
+        while node_q:
+            temp_nodes = collections.deque()
+            temp_path = collections.deque()
             
-            for i in range(len(choices[choice])):
-                backtrack(choices, choices[choice][i], path + [choices[choice][i]])
-                
-        backtrack(graph, 0, [0])
-​
-        return res
+            while node_q:
+                curr_node = node_q.popleft()
+                curr_path = path_q.popleft()
+                if curr_node == len(graph) - 1:
+                    res.append(curr_path)
+                for node in graph[curr_node]:
+                    temp_nodes.append(node)
+                    temp_path.append(curr_path + [node])
+            
+            node_q = temp_nodes
+            path_q = temp_path
         
+        return res
+            
+            
 ​
+                    
+                    
+                    
+                
+        
+        
+        
