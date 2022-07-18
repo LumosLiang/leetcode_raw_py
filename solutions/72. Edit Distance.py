fe237b6fd3770a1617acc9eef42231ccacda7f45
lumos! <43442549+LumosLiang@users.noreply.github.com>
2022-07-18 22:07:45 +0800

    92 ms

--- solutions/72. Edit Distance.py
+++ solutions/72. Edit Distance.py
@@ -1,28 +1,16 @@
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
         
-        return self.DFS(word1, word2)
-    
-    def DFS(self, word1: str, word2: str):
-        
-        # minimum number of operations required to convert word1[i:] to word2[j:] 
         l1, l2 = len(word1), len(word2)
         
         @lru_cache(None)
-        def backtrack(i, j):
-            
-            # insert
-            if i == l1: return l2 - j
-            # delete
-            if j == l2: return l1 - i
-            
-            # ros -> ro
-            if word1[i] == word2[j]:
-                return backtrack(i + 1, j + 1)
-            else:
-                # aros -> dro       delete                  replace              insert
-                return 1 + min(backtrack(i, j + 1), backtrack(i + 1, j + 1), backtrack(i + 1, j))
-        
-        return backtrack(0, 0)
+        def dfs(i, j):    
 ​
-       
+            if i == l1: return l2 - j
+            if j == l2: return l1 - i
+​
+            if word1[i] == word2[j]: return dfs(i + 1, j + 1)
+​
+            if word1[i] != word2[j]: return 1 + min(dfs(i + 1, j), dfs(i + 1, j + 1), dfs(i, j + 1))
+            
+        return dfs(0, 0)
