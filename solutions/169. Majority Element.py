b120d1fa6e78c1ef60c58e5acac706e34b4e19c6
lumos! <43442549+LumosLiang@users.noreply.github.com>
2022-07-28 21:18:29 +0800

    956 ms

--- solutions/169. Majority Element.py
+++ solutions/169. Majority Element.py
@@ -1,7 +1,7 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        return self.sol1(nums)
-        
+        return self.sol4(nums)
+    # hash, brute force, divde and conquer
     # O(N), O(1)
     # moore voting
     def sol1(self, nums: List[int]):
@@ -39,10 +39,26 @@ class Solution:
         return major
     
     # sorting o(nlogn)
-    def majorityelement2(self, nums):
+    def sol3(self, nums):
         nums.sort()
         return nums[len(nums)//2]
-                
+    
+    # bit
+    # O(32 * N), O(1)
+    def sol4(self, nums):
+        bit = 32 * [0]
+        for num in nums:
+            for i in range(32):
+                if num >> (31 - i) & 1:
+                    bit[i] += 1
+        
+        ans, l = 0, len(nums)
+        
+        for i in range(32):
+            if bit[i] > l // 2:
+                ans +=  1 << (31 - i)
+                # ans += 1 * 2 ** (31 - i)
+        return ans
     
 ​
         
