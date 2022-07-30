b8534122fdf9a27528a35a158abfc084d08b2cba
lumos! <43442549+LumosLiang@users.noreply.github.com>
2022-07-30 12:04:13 +0800

    53 ms

--- solutions/190. Reverse Bits.py
+++ solutions/190. Reverse Bits.py
@@ -1,9 +1,9 @@
 class Solution:
     def reverseBits(self, n: int) -> int:
+        # 取bit 然后算
+        res = 0
+        for i in range(31, -1, -1):
+            temp = n >> i & 1
+            res += temp * (1 << (31 - i))
         
-        out = 0
-        for i in range(32):
-            out = (out << 1)
-            out = out | (n & 1)
-            n >>= 1
-        return out
+        return res 
