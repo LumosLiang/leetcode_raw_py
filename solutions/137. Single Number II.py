8ac3ae14b5e50368a001475516f8fbb9ccaf0a1e
lumos! <43442549+LumosLiang@users.noreply.github.com>
2022-07-30 18:34:49 +0800

    283 ms

--- solutions/137. Single Number II.py
+++ solutions/137. Single Number II.py
@@ -1,13 +1,24 @@
 class Solution:
-    def singleNumber(self, nums):
-        x1, x2, mask = 0,0,0
+    def singleNumber(self, nums: List[int]) -> int:
         
-        for i in nums:
-            x2 ^= (x1 & i)
-            x1 ^= i
-            mask = ~(x1 & x2);
-            x2 &= mask;
-            x1 &= mask
+        # sort, hash
+        # bit
+        bit = [0] * 32
         
-        return x1
+        for num in nums:
+            for i in range(32):
+                if num >> i & 1:
+                    bit[31 - i] += 1
         
+        for i in range(32):
+            if bit[i] % 3:
+                bit[i] = 1
+            else:
+                bit[i] = 0
+        
+        ans = 0
+        if bit[0]: ans = -1 << 31
+        for i in range(1, 32):
+            ans += bit[i] * 1 << (31 - i)
+            
+        return ans
