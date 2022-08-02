89fe665655172bcaf284b1a331f52f88f576d813
lumos! <43442549+LumosLiang@users.noreply.github.com>
2022-08-02 15:17:38 +0800

    110 ms

--- solutions/557. Reverse Words in a String III.py
+++ solutions/557. Reverse Words in a String III.py
@@ -1,6 +1,9 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
-        
+    
+        return self.sol2(s)
+    
+    def sol1(self, s):
         s = s.split(" ")
         res = ""
         for i in range(len(s)):
@@ -8,4 +11,28 @@ class Solution:
             if i < len(s) - 1:res += " "
         
         return res
+    
+    def sol2(self, s):
+        
+        def reverse(s, left, right):
+            if left >= right: return
             
+            while left <= right:
+                s[left], s[right] = s[right], s[left]
+                left += 1
+                right -= 1
+        
+        s, p1, p2 = list(s) + [" "], 0, 0
+​
+        while p2 < len(s):
+            if s[p2] != " ":
+                p2 += 1
+            else:
+                reverse(s, p1, p2 - 1)
+                p2 += 1
+                p1 = p2
+                
+        
+        return "".join(s).rstrip(" ")    
+    
+    
