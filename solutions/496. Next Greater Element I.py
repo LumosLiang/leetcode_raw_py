51f84d2290a8c6720f3147d3c0092368209c5406
lumos! <43442549+LumosLiang@users.noreply.github.com>
2022-08-03 12:12:54 +0800

    62 ms

--- solutions/496. Next Greater Element I.py
+++ solutions/496. Next Greater Element I.py
@@ -1,6 +1,9 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
         
+        return self.sol2(nums1, nums2)
+        
+    def sol1(self, nums1: List[int], nums2: List[int]):
         s = []
         table = collections.defaultdict()
         
@@ -16,8 +19,7 @@ class Solution:
             if n in table: res.append(table[n])
         return res
         
-        
-        # 
+    
         # right to left
         # compare with lastseen biggest numbers with the curr one
             # if lastseen > curr, then it is the last one, then put itself into stack.
@@ -27,6 +29,18 @@ class Solution:
             # store the closest(Next) larger number
             # and store them in the small to large order(top to bottom)
         
-            
-            
-            
+    def sol2(self, nums1, nums2):
+        s = []
+        table = collections.defaultdict()
+        
+        for i, val in enumerate(nums2):
+            while s and nums2[s[-1]] < val:
+                j = s.pop()
+                table[nums2[j]] = val
+            s.append(i)
+        
+        res = []
+        for n in nums1:
+            if n in table: res.append(table[n])
+            else: res.append(-1)
+        return res
