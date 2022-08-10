76e62ea73d6d29acc3012834d439d501b136abd5
lumos! <43442549+LumosLiang@users.noreply.github.com>
2022-08-10 17:49:51 +0800

    52 ms

--- solutions/451. Sort Characters By Frequency.py
+++ solutions/451. Sort Characters By Frequency.py
@@ -1,17 +1,29 @@
-import collections 
-import heapq
 class Solution:
     def frequencySort(self, s: str) -> str:
+        return self.sol2(s)
+    def sol1(self, s):
+        fre = collections.Counter(s)
+        fre = [[-v, k] for k, v in fre.items()]
+        fre.sort()
+        
+        res = ""
+        
+        for f in fre:
+            res += f[1] * -f[0]
+        
+        return res
+    
+    def sol2(self, s):
         
         dic = collections.Counter(s)
-            
+        
         heap = []
         for k, v in dic.items():
-            heapq.heappush(heap, (v, k))
+            heapq.heappush(heap, (-v, k))
             
         res = ''
         for _ in range(len(heap)):
             v, k = heapq.heappop(heap)
-            res = k * v + res
-        
+            res += k * -v
+            
         return res
