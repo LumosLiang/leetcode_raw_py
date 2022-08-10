a89a4d970368235eaa7e1edbb0b5ee1b32a1affd
lumos! <43442549+LumosLiang@users.noreply.github.com>
2022-08-10 17:41:59 +0800

    101 ms

--- solutions/347. Top K Frequent Elements.py
+++ solutions/347. Top K Frequent Elements.py
@@ -1,14 +1,33 @@
-from collections import Counter
-import heapq
-​
 class Solution:
-    def topKFrequent(self, nums, k):
-        res = []
-        dict_nums = Counter(nums)
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        return self.heap(nums, k)
+    
+    # O(N + NlogN)
+    def hash_sort():
+        pass
+    
+    # O(N + mlogk), O(N)
+    def heap(self, nums, k):
+        h = collections.Counter(nums)
+        freq_idxs = [(v, k) for k, v in h.items()]
         
-        maxheap = [(-val, key) for key, val in dict_nums.items()]
-        heapq.heapify(maxheap)
-​
-        for _ in range(k):
-            res.append(heapq.heappop(maxheap)[1])
-        return res
+        res = []
+        
+        for fi in freq_idxs:
+            if len(res) < k:
+                heapq.heappush(res, fi)
+            else:
+                heapq.heappushpop(res, fi)
+        
+        return [r[1] for r in res]
+    
+    
+#     heappushpop -> 弹出的一定是最小的
+#     heapreplace -> 弹出的是上一个定点。
+    
+    
+    # O(N), O(N)
+    # guaranteed that the answer is unique
+    def quick_select():
+        
+        return
