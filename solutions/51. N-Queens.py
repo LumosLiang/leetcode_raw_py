1240ae23c29d26a4c574047f633df1e68f0bcd99
lumos! <43442549+LumosLiang@users.noreply.github.com>
2022-08-17 12:20:10 +0800

    Update 51. N-Queens.py

--- solutions/51. N-Queens.py
+++ solutions/51. N-Queens.py
@@ -25,9 +25,41 @@ class Solution:
                         board[x][y] = "."
                     else:
                         new_choices.append((x, y))
-​
+                      
                 backtrack(new_choices, board, queencnt + 1, cnt)
                 board[m][n] = "."
                 
         backtrack(choices, board, 0, n)
         return res
+    
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        
+        def check(board, choice_idx):
+            for i in range(n):
+                if i != choice_idx and board[i] != -1:
+                    if board[choice_idx] == board[i] or abs(board[i] - board[choice_idx]) == abs(i - choice_idx):
+                        return False
+            return True
+        
+        res = []
+        
+        def dfs(i, board, path):
+        
+            if i == n:
+                res.append(path)
+                return
+                
+            for j in range(n):
+                board[i] = j
+                if check(board, i):
+                    temp = "."*j + "Q" + "."*(n - j - 1)
+                    dfs(i + 1, board, path + [temp])
+                board[i] = -1
+                
+        dfs(0, [-1] * n, [])
+        return res
+
+        
+
+    
