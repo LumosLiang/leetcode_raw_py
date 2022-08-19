cb22b9fce4bf1caf06f6e2510ad57e1b17907632
lumos! <43442549+LumosLiang@users.noreply.github.com>
2022-08-19 12:32:48 +0800

    48 ms

--- solutions/309. Best Time to Buy and Sell Stock with Cooldown.py
+++ solutions/309. Best Time to Buy and Sell Stock with Cooldown.py
@@ -1,7 +1,7 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         
-        # DP 答案要什么，我的状态就是什么
+                    
         
         # i - 1  hold, sell, cooldown, 
         
@@ -13,9 +13,7 @@ class Solution:
         
         for i in range(1, len(prices)):
             dp[i][0] = max(dp[i - 1][0], dp[i - 1][2] - prices[i], -prices[i])
-            dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] + prices[i])
+            dp[i][1] = dp[i - 1][0] + prices[i]
             dp[i][2] = max(dp[i - 1][2], dp[i - 1][1])
         
         return max(dp[-1])
-        
-        
