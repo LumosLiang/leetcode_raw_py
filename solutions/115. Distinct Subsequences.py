2fe658d2ff9f5ae346f6d7c0ba4cc5f5898f9221
lumos! <43442549+LumosLiang@users.noreply.github.com>
2022-08-23 17:31:29 +0800

    667 ms

---
+++ solutions/115. Distinct Subsequences.py
@@ -0,0 +1,22 @@
+class Solution:
+    def numDistinct(self, s: str, t: str) -> int:
+        
+        l1, l2 = len(s), len(t)
+        
+        @lru_cache(None)
+        def dfs(i, j):
+            
+            if j == l2: return 1
+            
+            if i < l1 and j < l2:
+                if s[i] == t[j]:
+                
+                    return dfs(i + 1, j + 1) + dfs(i + 1, j)
+            
+                else:
+                
+                    return dfs(i + 1, j)
+            return 0
+            
+            
+        return dfs(0, 0)
