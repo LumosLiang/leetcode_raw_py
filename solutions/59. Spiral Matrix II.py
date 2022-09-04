6397c98433c969ea99540555371b3e66b90909aa
lumos! <43442549+LumosLiang@users.noreply.github.com>
2022-09-04 22:54:19 +0800

    36 ms

--- solutions/59. Spiral Matrix II.py
+++ solutions/59. Spiral Matrix II.py
@@ -1,20 +1,53 @@
 class Solution:
-    def generateMatrix(self, n: int):
+    def generateMatrix(self, n: int) -> List[List[int]]:
         
-        x, y, dx, dy = 0, 0, 1, 0
-        val = 1
-        res = [[0 for i in range(n)] for j in range(n)]
+        matrix = [[0 for i in range(n)] for j in range(n)]
+        l, r, u, d = 0, n - 1, 0, n - 1
+        cnt = 1
+        
+        while cnt < n ** 2 + 1:
+        
+            for i in range(l, r + 1):
+                matrix[l][i] = cnt
+                cnt += 1
+                
+            u += 1
+            for i in range(u, d + 1):
+                matrix[i][r] = cnt
+                cnt += 1
+                
+            r -= 1
+            for i in range(r, l - 1, -1):
+                matrix[d][i] = cnt
+                cnt += 1
+​
+            d -= 1
+            for i in range(d, u - 1, -1):
+                matrix[i][l] = cnt
+                cnt += 1
+​
+            l += 1
+            
+        return matrix
+​
+class Solution:
+    def generateMatrix(self, n: int) -> List[List[int]]:
+        
+        matrix = [[0 for i in range(n)] for j in range(n)]
+        
+        cnt = 1
+        x, y, dx, dy = 0, 0, 0, 1
         visited = set()
         
-        while val < n ** 2 + 1:
+        while cnt < n ** 2 + 1:
             
-            if not 0 <= x + dx < n or not 0 <= y + dy < n or (y + dy, x + dx) in visited:
-                dx, dy = -dy, dx
+            if x + dx in [-1, n] or y + dy in [-1, n] or (x + dx, y + dy) in visited:
+                dx, dy = dy, -dx 
             
-            res[y][x] = val
-            visited.add((y, x))
+            matrix[x][y] = cnt
+            visited.add((x, y))
+            cnt += 1
+            x += dx
+            y += dy
             
-            x, y = x + dx, y + dy
-            val += 1
-        
-        return res
+        return matrix
