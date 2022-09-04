b93794add4c1672fdabc49e68fb8ff338e45eed7
lumos! <43442549+LumosLiang@users.noreply.github.com>
2022-09-04 18:00:07 +0800

    39 ms

--- solutions/69. Sqrt(x).py
+++ solutions/69. Sqrt(x).py
@@ -1,13 +1,41 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
-        if x == 0: return 0
-        lo, hi = 0, x 
         
-        while lo <= hi:
-            mid = lo + (hi - lo) // 2
-            if mid ** 2 == x: return mid
-            elif mid ** 2 > x: hi = mid - 1
-            else: lo = mid + 1 
+        return self.sol1(x)
+        
+    def sol1(self, x):
+        l, r = 0, x
+        
+        
+        while l < r:
+            mid = (l + r + 1) // 2
+            val = mid ** 2
             
-        return hi
+            if val <= x:
+                l = mid 
+            else:
+                r = mid - 1
         
+        return l
+    
+    def sol3(self, x):
+        l, r = 0, x
+        
+        while l < r:
+            mid = (l + r) // 2
+            val = mid ** 2
+            
+            if val < x:
+                l = mid + 1 
+            else:
+                r = mid
+        
+        return l if l ** 2 == x else l - 1
+​
+    # follow-up: if require the accuracy to decimal or more
+    def sol2(self, x, k):
+    
+        def find_interger(x):
+            l, r = 0, x
+​
+            while l < r:
