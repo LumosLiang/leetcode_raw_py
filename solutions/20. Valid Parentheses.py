a9fd3a1fb58e498780610f9613100e86dfd32a06
lumos! <43442549+LumosLiang@users.noreply.github.com>
2022-10-10 09:48:53 +0800

    53 ms

--- solutions/20. Valid Parentheses.py
+++ solutions/20. Valid Parentheses.py
@@ -1,29 +1,20 @@
 class Solution:
-<<<<<<< HEAD
-    def isValid(self, s: str) -> bool:
-
-        stack = []
-        hash = {'(': ')', '{': '}', '[': ']'}
-
-        for item in s:
-            if stack and stack[-1] in hash and hash[stack[-1]] == item:
-                stack.pop()
-                continue
-            stack.append(item)
-
-        return not stack
-=======
     def isValid(self, s: str) -> bool:
         
-        d = {'(':')', '{':'}', '[':']'}
+        if s == "": return False
         
+        d = {'(':')','{':'}','[':']'}
+​
         stack = []
         
-        for i in range(len(s)):
-            if stack and stack[-1] in d and d[stack[-1]] == s[i]:
+        for char in s:        
+            if stack and stack[-1] in d and char == d[stack[-1]]:
                 stack.pop()
-            else: stack.append(s[i])
-                
-        if not stack: return True
-        else: return False
->>>>>>> 1870dd3c764dee6f244fb461499fe34e38527ffd
+            else:
+                stack.append(char)
+        
+        return not stack
+            
+            
+            
+            
