cbb4313ce8267a697c172922b60258ad287f6977
lumos! <43442549+LumosLiang@users.noreply.github.com>
2022-10-10 08:58:58 +0800

    109 ms

--- solutions/80. Remove Duplicates from Sorted Array II.py
+++ solutions/80. Remove Duplicates from Sorted Array II.py
@@ -11,3 +11,19 @@ class Solution:
                 hash[nums[l]] += 1
             
         return l + 1
+    
+    def sol1(self, nums):
+        l = 0
+        cnt = 1
+        
+        for r in range(1, len(nums)):
+            if nums[r] != nums[l]:        
+                l += 1
+                nums[l] = nums[r]
+                cnt = 1
+            elif nums[r] == nums[l] and cnt < 2:
+                l += 1
+                nums[l] = nums[r]
+                cnt += 1
+            
+        return l + 1
